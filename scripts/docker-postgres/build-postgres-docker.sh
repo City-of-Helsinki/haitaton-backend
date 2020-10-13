37fdbaa96371e499deccf09310bda0ac2ce6a18c
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-10-13 11:12:22 +0300

    HAI-28 added removing a container if exists to local dev postgis

--- scripts/docker-postgres/build-postgres-docker.sh
+++ scripts/docker-postgres/build-postgres-docker.sh
@@ -1,3 +1,5 @@
 docker build -t haitaton-postgres .
+#remove old container if exists
+docker rm -f haitaton-postgres > /dev/null 2>&1 && echo 'removed container' || echo 'nothing to remove'
 #add your own data path and port, I use 5433 here and ~/haitaton-data
 docker run -d --rm --name haitaton-postgres -e POSTGRES_PASSWORD=postgres -p 5439:5432 --volume ~/haitaton-data:/data haitaton-postgres
