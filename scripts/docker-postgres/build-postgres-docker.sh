45af440e68939e3bd0a14e078e6c7faa6320e70f
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-10-13 09:46:22 +0300

    HAI-42 added scripts and Dockerfile for setting up docker container with postgres and postgis

---
+++ scripts/docker-postgres/build-postgres-docker.sh
@@ -0,0 +1,3 @@
+docker build -t haitaton-postgres .
+#add your own data path and port, I use 5433 here and ~/haitaton-data
+docker run --rm --name haitaton-postgres -e POSTGRES_PASSWORD=postgres -p 5439:5432 --volume ~/haitaton-data:/data haitaton-postgres
