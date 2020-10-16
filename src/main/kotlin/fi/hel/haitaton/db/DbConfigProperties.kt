144e2026cef15190652b06bb1afadecdde0493ac
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-10-16 10:22:59 +0300

    HAI-42 fixed application properties, removed redundant props

---
+++ src/main/kotlin/fi/hel/haitaton/db/DbConfigProperties.kt
@@ -0,0 +1,17 @@
+package fi.hel.haitaton.db
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class DbConfigProperties {
+
+    @Value("\${spring.datasource.url}")
+    lateinit var appDatasourceUrl: String
+
+    @Value("\${spring.datasource.username}")
+    lateinit var appDatasourceUsername: String
+
+    @Value("\${spring.datasource.password}")
+    lateinit var appDatasourcePassword: String
+}
