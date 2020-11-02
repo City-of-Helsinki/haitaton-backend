70bd62f84eb7bebafc6b2cd29171b751b9ab5296
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2020-11-02 14:23:13 +0200

    HAI-160: refactored packages and moved Application (was HelloApplication) to correct place

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Application.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HelloApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<HelloApplication>(*args)
+    runApplication<Application>(*args)
 }
