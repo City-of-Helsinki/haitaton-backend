99fce6107ed3a4d4568696272fc60df5049940bf
tintotti <68274930+tintotti@users.noreply.github.com>
2020-11-02 14:49:14 +0200

    HAI-156 HankeController first version with tests (#32)
    * First api for getting objects from HankeController
    
    * HAI-156 Integration tests and some changes to fix the context
    
    * Juniper dependency removed from build.gradle.kts
    
    * HAI-156 Unnecessary imports removed

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hello/HelloApplication.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hello/HelloApplication.kt
@@ -2,10 +2,12 @@ package fi.hel.haitaton.hello
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("fi.hel.haitaton")
 class HelloApplication
 
 fun main(args: Array<String>) {
-	runApplication<HelloApplication>(*args)
+    runApplication<HelloApplication>(*args)
 }
