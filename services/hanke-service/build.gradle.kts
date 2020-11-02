ed1aea83a09700c28bb209cb5047d4e38b529646
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2020-11-02 09:06:14 +0200

    HAI-90: added Actuator info with basic build and java version info

--- services/hanke-service/build.gradle.kts
+++ services/hanke-service/build.gradle.kts
@@ -39,6 +39,10 @@ idea {
 	}
 }
 
+springBoot {
+	buildInfo()
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
