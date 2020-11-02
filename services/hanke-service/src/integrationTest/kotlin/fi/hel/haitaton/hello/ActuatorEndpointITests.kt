f1b38b378ccd62bc69891b3111bc1dca65d8fb0e
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2020-11-02 10:26:48 +0200

    HAI-90: added integration tests for Actuator endpoints

---
+++ services/hanke-service/src/integrationTest/kotlin/fi/hel/haitaton/hello/ActuatorEndpointITests.kt
@@ -0,0 +1,49 @@
+package fi.hel.haitaton.hello
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+
+
+/**
+ * For testing Spring Boot Actuator endpoints
+ */
+@SpringBootTest(properties = ["management.server.port="])
+@AutoConfigureMockMvc
+@EnableAutoConfiguration
+class ActuatorEndpointITests(@Autowired val mockMvc: MockMvc) {
+
+    @Test
+    fun readiness() {
+        mockMvc
+                .perform(get("/actuator/health/readiness"))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType("application/vnd.spring-boot.actuator.v3+json"))
+                .andExpect(jsonPath("$.status").value("UP"))
+    }
+
+    @Test
+    fun liveness() {
+        mockMvc
+                .perform(get("/actuator/health/liveness"))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType("application/vnd.spring-boot.actuator.v3+json"))
+                .andExpect(jsonPath("$.status").value("UP"))
+    }
+
+    @Test
+    fun info() {
+        mockMvc
+                .perform(get("/actuator/info"))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType("application/vnd.spring-boot.actuator.v3+json"))
+                .andExpect(jsonPath("$.build.artifact").value("hanke-service"))
+    }
+}
