30719a4b3c689d7f5d3064ee4b40bf62ac014004
tintotti <68274930+tintotti@users.noreply.github.com>
2020-11-02 14:49:14 +0200

    HAI-156 HankeController first version with tests (#32)
    * First api for getting objects from HankeController
    
    * HAI-156 Integration tests and some changes to fix the context
    
    * Juniper dependency removed from build.gradle.kts
    
    * HAI-156 Unnecessary imports removed

---
+++ services/hanke-service/src/test/kotlin/fi/hel/haitaton/controllers/HankeControllerTest.kt
@@ -0,0 +1,32 @@
+package fi.hel.haitaton.controllers
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+internal class HankeControllerTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun `test that the getHankebyId returns ok`() {
+        var response: ResponseEntity<Any>
+        //todo: mock with data when implementation further
+
+        // Dummy call:
+        response = HankeController().getHankeById("koira")
+        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(response.body).isNotNull
+
+    }
+}
