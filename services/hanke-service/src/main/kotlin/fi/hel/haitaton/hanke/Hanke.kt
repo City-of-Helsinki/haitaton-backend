355593c5b286505e3037052355ac4a0d0e81d332
tintotti <68274930+tintotti@users.noreply.github.com>
2020-11-06 15:16:04 +0200

    HAI-133 Controller contains GET/POST/PUT methods for first page use (#34)
    * HAI-133 Put method implemented and tests for it, Hanke has optional attributes
    * HankeService introduced from Controller need point of view.
    * Stub for HankeService, Integration tests are working with mocked service values
    After code review:
    * Mock instead of Spy, open removed, interface for service added
    * ResponseEntity return value to Any as per codereview comments

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Hanke.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Hanke.kt
@@ -2,11 +2,15 @@ package fi.hel.haitaton.hanke
 
 import java.time.ZonedDateTime
 
+/**
+ * When creating Hanke, only owner is mandatory.
+ */
 data class Hanke(
-        var hankeId: String,
-        val name: String,
-        val implStartDate: ZonedDateTime,
-        val implEndDate: ZonedDateTime,
+        var hankeId: String?,
+        var isYKTHanke: Boolean?,
+        var name: String?,
+        var startDate: ZonedDateTime?,
+        var endDate: ZonedDateTime?,
         val owner: String,
-        val phase: Int)
+        var phase: Int?)
 
