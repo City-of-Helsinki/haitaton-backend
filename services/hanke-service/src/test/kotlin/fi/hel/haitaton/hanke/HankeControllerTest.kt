aa5fd28ff66de7cfbd16c0fe5484bb919a1e393f
tintotti <68274930+tintotti@users.noreply.github.com>
2020-11-06 15:16:04 +0200

    HAI-133 Controller contains GET/POST/PUT methods for first page use (#34)
    * HAI-133 Put method implemented and tests for it, Hanke has optional attributes
    * HankeService introduced from Controller need point of view.
    * Stub for HankeService, Integration tests are working with mocked service values
    After code review:
    * Mock instead of Spy, open removed, interface for service added
    * ResponseEntity return value to Any as per codereview comments

--- services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/HankeControllerTest.kt
+++ services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/HankeControllerTest.kt
@@ -5,27 +5,64 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 
 import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import java.time.ZonedDateTime
 
-internal class HankeControllerTest {
+@SpringBootTest
+class HankeControllerTest {
+
+    val mockedHankeId = "AFC1234"
+
+    @InjectMocks
+    lateinit var hankeController: HankeController
+
+    @MockBean
+    lateinit var hankeService: HankeService
 
     @BeforeEach
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        Mockito.`when`(hankeService.loadHanke(mockedHankeId))
+                .thenReturn(Hanke(mockedHankeId, true, "Mannerheimintien remontti remonttinen", ZonedDateTime.now(), ZonedDateTime.now(), "Risto", 1))
     }
 
     @AfterEach
     fun tearDown() {
     }
 
+
     @Test
     fun `test that the getHankebyId returns ok`() {
-        //todo: mock with data when implementation further
+        //  mockHankeService
+        val response: ResponseEntity<Any> = hankeController.getHankeById(mockedHankeId)
 
-        // Dummy call:
-        val response: ResponseEntity<Hanke> = HankeController().getHankeById("koira")
         Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
         Assertions.assertThat(response.body).isNotNull
+        Assertions.assertThat((response.body as Hanke).name).isNotEmpty()
+    }
 
+    @Test
+    fun `test that the putHanke can be called with partial hanke data`() {
+        //TODO: mock with data when implementation further
+
+        // Dummy call:
+        var partialHanke = Hanke(hankeId = "id123", name = "hankkeen nimi", isYKTHanke = false, startDate = null, endDate = null, owner = "Tiina", phase = null)
+        //mockHankeService
+
+        val response: ResponseEntity<Any> = hankeController.createPartialHanke(partialHanke, "id123")
+
+        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(response.body).isNotNull
+        var responseHanke = response as? ResponseEntity<Hanke>
+        Assertions.assertThat(responseHanke?.body).isNotNull
+        Assertions.assertThat(responseHanke?.body?.name).isEqualTo("hankkeen nimi")
     }
 }
