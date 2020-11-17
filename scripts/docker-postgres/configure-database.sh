f316e86d050b483c62e3041ff82f61369fec5be9
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-11-17 10:40:27 +0200

    HAI-221 moved creating extension to correct place in script

--- scripts/docker-postgres/configure-database.sh
+++ scripts/docker-postgres/configure-database.sh
@@ -11,10 +11,12 @@ set -euo pipefail
 #POSTGRES_PASSWORD=postgres
 
 echo "Creating database \"$DB_APP_DB\", creating role \"$DB_APP_USER\" with database owner privileges…"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname template1 -c 'create extension postgis;'
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-END
+create extension postgis;
 create role "${DB_APP_USER}" with password '${DB_APP_PASSWORD}' login;
+alter user "${DB_APP_USER}" with superuser;
+alter user "${DB_APP_USER}" with superuser;
 create database "${DB_APP_DB}" encoding 'UTF-8';
 create database "${DB_APP_DB_TEST}" encoding 'UTF-8';
 grant all privileges on database "${DB_APP_DB}" to "${DB_APP_USER}";
