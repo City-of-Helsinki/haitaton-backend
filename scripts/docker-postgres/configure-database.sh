cb7edbf4d297b70417e51c560952834df98c124f
Anne-Mari Silvast <annemari.silvast@gmail.com>
2020-11-17 10:44:58 +0200

    HAI-221 added postgis extension to docker (#41)
    * HAI-221 added postgis extension to docker
    
    * HAI-221 moved creating extension to correct place in script

--- scripts/docker-postgres/configure-database.sh
+++ scripts/docker-postgres/configure-database.sh
@@ -13,7 +13,10 @@ set -euo pipefail
 echo "Creating database \"$DB_APP_DB\", creating role \"$DB_APP_USER\" with database owner privileges…"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-END
+create extension postgis;
 create role "${DB_APP_USER}" with password '${DB_APP_PASSWORD}' login;
+alter user "${DB_APP_USER}" with superuser;
+alter user "${DB_APP_USER}" with superuser;
 create database "${DB_APP_DB}" encoding 'UTF-8';
 create database "${DB_APP_DB_TEST}" encoding 'UTF-8';
 grant all privileges on database "${DB_APP_DB}" to "${DB_APP_USER}";
