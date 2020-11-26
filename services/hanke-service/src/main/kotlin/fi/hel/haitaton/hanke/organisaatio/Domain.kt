3326eecf75c9f8c970f17e8da5b655fc27584021
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-11-26 12:42:58 +0200

    HAI-233 removed createdAt and modifiedAt from being sent to frontend

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/organisaatio/Domain.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/organisaatio/Domain.kt
@@ -1,13 +1,9 @@
 package fi.hel.haitaton.hanke.organisaatio
 
-import java.time.ZonedDateTime
-
 data class Organisaatio(
     var id: Int? = null,
-    var organisaatioTunnus: String? = null,
-    var organisaatioNimi: String? = null,
-    var createdAt: ZonedDateTime? = null,
-    var updatedAt: ZonedDateTime? = null
+    var tunnus: String? = null,
+    var nimi: String? = null
 )
 
 class OrganisaatioNotFoundException() : RuntimeException("Cannot find organizations")
--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/organisaatio/OrganisaatioServiceImpl.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/organisaatio/OrganisaatioServiceImpl.kt
@@ -1,10 +1,8 @@
 package fi.hel.haitaton.hanke.organisaatio
 
-import fi.hel.haitaton.hanke.*
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.time.ZonedDateTime
 
 private val logger = KotlinLogging.logger { }
 
@@ -21,9 +19,7 @@ class OrganisaatioServiceImpl(@Autowired val organisaatioRepository: Organisaati
         val o = Organisaatio(
             organisaatioEntity.id,
             organisaatioEntity.organisaatioTunnus,
-            organisaatioEntity.nimi,
-            if (organisaatioEntity.createdAt != null) ZonedDateTime.of(organisaatioEntity.createdAt, TZ_UTC) else null,
-            if (organisaatioEntity.modifiedAt != null) ZonedDateTime.of(organisaatioEntity.modifiedAt, TZ_UTC) else null,
+            organisaatioEntity.nimi
 
         )
         return o
