340ad6857656ec573a71980636f17cf38e8a35ac
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2020-12-11 10:28:40 +0200

    HAI-324: testing whether ansible deploy really uses the updated images

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Application.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Application.kt
@@ -19,11 +19,13 @@ private val logger = KotlinLogging.logger { }
 
 fun main(args: Array<String>) {
     // FIXME This is only for debugging in OpenShift where it does not seem to be able to connect to the database for some reason
+    /*
     logger.info {
         buildString {
             append("Environment:\n")
             append(System.getenv().entries.joinToString("\n") { "${it.key} = ${it.value}" })
         }
     }
+     */
     runApplication<Application>(*args)
 }
