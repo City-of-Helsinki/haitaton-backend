bc522bae08103c09b4c42638049bc2aa044953a4
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2021-01-12 13:32:12 +0200

    HAI-467: added CORS allowed-origins

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Application.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Application.kt
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.web.filter.ForwardedHeaderFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+
 
 @SpringBootApplication
 class Application {
@@ -12,6 +18,16 @@ class Application {
     @Bean
     fun forwardedHeaderFilter(): ForwardedHeaderFilter? {
         return ForwardedHeaderFilter()
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/hankkeet").allowedOrigins("http://localhost", "http://localhost:8000", "http://localhost:3001", "https://haitaton-ui-dev.agw.arodevtest.hel.fi")
+                registry.addMapping("/organisaatiot").allowedOrigins("http://localhost", "http://localhost:8000", "http://localhost:3001", "https://haitaton-ui-dev.agw.arodevtest.hel.fi")
+            }
+        }
     }
 }
 
