281cab8632d81159021b55fbf098f4a037ce5957
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2021-01-13 10:43:08 +0200

    HAI-467 CORS (#74)
    * HAI-467: Added CORS settings

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Application.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/Application.kt
@@ -1,17 +1,38 @@
 package fi.hel.haitaton.hanke
 
 import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.web.filter.ForwardedHeaderFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+
 
 @SpringBootApplication
 class Application {
 
+    @Value("\${haitaton.cors.allowedOrigins}")
+    lateinit var allowedOrigins: String
+
     @Bean
     fun forwardedHeaderFilter(): ForwardedHeaderFilter? {
         return ForwardedHeaderFilter()
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/hankkeet").allowedMethods("POST", "GET").allowedOrigins(allowedOrigins)
+                registry.addMapping("/hankkeet/**").allowedMethods("POST", "GET", "PUT").allowedOrigins(allowedOrigins)
+                registry.addMapping("/organisaatiot").allowedMethods("GET").allowedOrigins(allowedOrigins)
+            }
+        }
     }
 }
 
