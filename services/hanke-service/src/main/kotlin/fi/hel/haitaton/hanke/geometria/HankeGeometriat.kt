a3cb06143a527d2b5b4deb86d39b6ed382338e95
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2021-02-11 14:53:53 +0200

    HAI-646: include hankeTunnus in returned Hanke GeoJson Feature.properties (#93)
    HAI-646: include hankeTunnus in returned Hanke GeoJson Feature.properties

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/geometria/HankeGeometriat.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/geometria/HankeGeometriat.kt
@@ -1,20 +1,46 @@
 package fi.hel.haitaton.hanke.geometria
 
+import fi.hel.haitaton.hanke.HankeEntity
+import fi.hel.haitaton.hanke.domain.Hanke
 import org.geojson.FeatureCollection
 import java.time.ZonedDateTime
 
 data class HankeGeometriat(
-        var id: Int? = null,
-        var hankeId: Int? = null,
-        var featureCollection: FeatureCollection? = null,
-        var version: Int? = null,
-        var createdByUserId: String? = null,
-        var createdAt: ZonedDateTime? = null,
-        var modifiedByUserId: String? = null,
-        var modifiedAt: ZonedDateTime? = null
+    var id: Int? = null,
+    var hankeId: Int? = null,
+    var featureCollection: FeatureCollection? = null,
+    var version: Int? = null,
+    var createdByUserId: String? = null,
+    var createdAt: ZonedDateTime? = null,
+    var modifiedByUserId: String? = null,
+    var modifiedAt: ZonedDateTime? = null
 ) {
     fun withFeatureCollection(featureCollection: FeatureCollection): HankeGeometriat {
         this.featureCollection = featureCollection
         return this
+    }
+
+    fun includeHankeProperties(hanke: Hanke) {
+        this.featureCollection?.let { featureCollection ->
+            featureCollection.features.forEach { feature ->
+                if (feature.properties == null) {
+                    feature.properties = mutableMapOf()
+                }
+                feature.properties["hankeTunnus"] = hanke.hankeTunnus
+                // Add here other properties when needed
+            }
+        }
+    }
+
+    fun includeHankeProperties(hanke: HankeEntity) {
+        this.featureCollection?.let { featureCollection ->
+            featureCollection.features.forEach { feature ->
+                if (feature.properties == null) {
+                    feature.properties = mutableMapOf()
+                }
+                feature.properties["hankeTunnus"] = hanke.hankeTunnus
+                // Add here other properties when needed
+            }
+        }
     }
 }
