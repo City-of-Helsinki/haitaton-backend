b5d8608f871fde4eb943b1bd432cb8ba046a19c5
tintotti <tiina.m.poyhonen@gmail.com>
2021-02-12 09:58:15 +0200

    Started draftin katuluokka decision making

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/tormaystarkastelu/TormaystarkasteluPaikkaServiceImpl.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/tormaystarkastelu/TormaystarkasteluPaikkaServiceImpl.kt
@@ -1,9 +1,8 @@
 package fi.hel.haitaton.hanke.tormaystarkastelu
 
 import fi.hel.haitaton.hanke.domain.Hanke
-import java.util.function.Consumer
 
-class TormaystarkasteluPaikkaServiceImpl : TormaystarkasteluPaikkaService {
+class TormaystarkasteluPaikkaServiceImpl (private val tormaystarkasteluDao: TormaystarkasteluDao): TormaystarkasteluPaikkaService {
 
     /**
      * Returns luokittelutulos list for hanke based on its hankeGeometria comparison to the different map references
@@ -35,17 +34,79 @@ class TormaystarkasteluPaikkaServiceImpl : TormaystarkasteluPaikkaService {
         if (hankeGeometriatId == null)
             return katuLuokittelu
 
-        val tormaystarkasteluYlreParts = ""   //TODO: call dao check to get the priority/main
-        val tormaystarkasteluYlreClasses = ""   //TODO: call dao check to get the priority/main
+        val tormaystarkasteluYlreParts = ""   //TODO: call dao check to get the matches
+        val tormaystarkasteluYlreClasses = ""   //TODO: call dao check to get the matches
 
 
-        if (!hitsInYlreParts() && !hitsInYlreClass() ) {
+        if (!hitsInYlreParts(tormaystarkasteluYlreParts) && !hitsInYlreClass(tormaystarkasteluYlreClasses)) {
             katuLuokittelu.add(Luokittelutulos(hankeGeometriatId, LuokitteluType.KATULUOKKA, "0", KatuluokkaTormaysLuokittelu.ZERO.toString()))
+            return katuLuokittelu
+        }
+
+        val tormaystarkasteluStreetClasses = "" //TODO: call dao check to get the matches
+
+        val maximumStreetClass = maximumInStreetClasses(tormaystarkasteluStreetClasses)
+        if (maximumStreetClass != null) { //streetClass exits
+            //get max from streetclasses
+            if (maximumStreetClass == "5" || maximumStreetClass == "4" || maximumStreetClass == "3") {
+                //is it 3-5, use it and leave
+            } else {
+                //else (1-2)
+            }
+            val tormaystarkasteluCentralBusinessArea = "" //TODO: call dao check to get the matches
+
+            if (hitsInCentralBusinessArea(tormaystarkasteluCentralBusinessArea)) {
+                //arvo is 2 set, and leave
+                katuLuokittelu.add(Luokittelutulos(hankeGeometriatId, LuokitteluType.KATULUOKKA, "2", KatuluokkaTormaysLuokittelu.TWO.toString()))
+                return katuLuokittelu
+            } else {
+                //arvo is 1, set and leave
+                katuLuokittelu.add(Luokittelutulos(hankeGeometriatId, LuokitteluType.KATULUOKKA, "1", KatuluokkaTormaysLuokittelu.ONE.toString()))
+                return katuLuokittelu
+            }
+        }
+
+        if (!hitsInYlreClass(tormaystarkasteluYlreClasses)) {  //ylre_parts yes but still no hit in any usable classification
+            katuLuokittelu.add(Luokittelutulos(hankeGeometriatId, LuokitteluType.KATULUOKKA, "0", KatuluokkaTormaysLuokittelu.ZERO.toString()))
+            return katuLuokittelu
+        } else {
+            //is ylre_class hit 3-5
+            //use it 3-5 and leave
+
+            //else (1-2)
+            val tormaystarkasteluCentralBusinessArea = "" //TODO: call dao check to get the matches
+
+            if (hitsInCentralBusinessArea(tormaystarkasteluCentralBusinessArea)) {
+                //arvo is 2 set, and leave
+                katuLuokittelu.add(Luokittelutulos(hankeGeometriatId, LuokitteluType.KATULUOKKA, "2", KatuluokkaTormaysLuokittelu.TWO.toString()))
+                return katuLuokittelu
+            } else {
+                //arvo is 1, set and leave
+                katuLuokittelu.add(Luokittelutulos(hankeGeometriatId, LuokitteluType.KATULUOKKA, "1", KatuluokkaTormaysLuokittelu.ONE.toString()))
+                return katuLuokittelu
+            }
         }
 
 
 
+
         return katuLuokittelu
+    }
+
+    private fun maximumInStreetClasses(tormaystarkasteluStreetClasses: String): String {
+        TODO("Not yet implemented")
+    }
+
+    private fun hitsInCentralBusinessArea(tormaystarkasteluCentralBusinessArea: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    private fun hitsInYlreClass(tormaystarkasteluYlreClasses: String): Any {
+        TODO("Not yet implemented")
+    }
+
+    private fun hitsInYlreParts(tormaystarkasteluYlreParts: String): Any {
+        TODO("Not yet implemented")
     }
 
     internal fun getLiikennemaaraLuokitteluTulos(hanke: Hanke, rajaArvot: LuokitteluRajaArvot, katuluokkaLuokittelut: List<Luokittelutulos>): List<Luokittelutulos> {
@@ -60,7 +121,7 @@ class TormaystarkasteluPaikkaServiceImpl : TormaystarkasteluPaikkaService {
         //if no id let's get out of here
         if (hankeGeometriatId == null) return pyorailyLuokittelu
 
-        val tormaystarkastelutulos = ""   //TODO: call dao check to get the priority/main
+        val tormaystarkastelutulos = tormaystarkasteluDao.pyorailyreitit(hankeGeometriatId)
 
         if (matchesPriorityCycling(tormaystarkastelutulos)) {
             pyorailyLuokittelu.add(Luokittelutulos(hankeGeometriatId, LuokitteluType.PYORAILYN_PAAREITTI, "5", PyorailyTormaysLuokittelu.FIVE.toString()))
