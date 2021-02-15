1aac670ac17a60d21cb23c0ae6b2ad4b87f9ed36
muhassin <muhassin@obvious.com>
2021-02-15 13:25:12 +0200

    HAI-583 added hashCode and equals

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/domain/HankeTilat.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/domain/HankeTilat.kt
@@ -1,7 +1,6 @@
 package fi.hel.haitaton.hanke.domain
 
 // Note: not a data class because that would require adding at least one constructor parameter.
-// TODO: hashcode and equals etc.
 class HankeTilat {
     // NOTE: Flags are handled internally, and are not saved directly to database.
     // Instead, in order to change flag(s), one needs to either set the value(s) and call
@@ -14,5 +13,30 @@ class HankeTilat {
     var onLiikHaittaIndeksi: Boolean = false // Not saved to databased
     var onViereisiaHankkeita: Boolean = false // Use updateHankeStateFlags()
     var onAsiakasryhmia: Boolean = false
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is HankeTilat) return false
+
+        if (onGeometrioita != other.onGeometrioita) return false
+        if (onKaikkiPakollisetLuontiTiedot != other.onKaikkiPakollisetLuontiTiedot) return false
+        if (onTiedotLiikHaittaIndeksille != other.onTiedotLiikHaittaIndeksille) return false
+        if (onLiikHaittaIndeksi != other.onLiikHaittaIndeksi) return false
+        if (onViereisiaHankkeita != other.onViereisiaHankkeita) return false
+        if (onAsiakasryhmia != other.onAsiakasryhmia) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        // Crude, but likely never used for realz
+        var result = (if (onGeometrioita) 1 else 0)
+        result = 31 * result + (if (onKaikkiPakollisetLuontiTiedot) 1 else 0)
+        result = 31 * result + (if (onTiedotLiikHaittaIndeksille) 1 else 0)
+        result = 31 * result + (if (onLiikHaittaIndeksi) 1 else 0)
+        result = 31 * result + (if (onViereisiaHankkeita) 1 else 0)
+        result = 31 * result + (if (onAsiakasryhmia) 1 else 0)
+        return result
+    }
 
 }
