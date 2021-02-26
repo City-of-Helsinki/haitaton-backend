e71bad6dd240f59c0ea41c1227966fe4b5d72caa
tintotti <tiina.m.poyhonen@gmail.com>
2021-02-26 14:25:05 +0200

    More unit test cases for joukkoliikenneindeksi calculation

--- services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/tormaystarkastelu/TormaystarkasteluCalculatorTest.kt
+++ services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/tormaystarkastelu/TormaystarkasteluCalculatorTest.kt
@@ -14,7 +14,14 @@ internal class TormaystarkasteluCalculatorTest {
                 LuokitteluType.PYORAILYN_PAAREITTI,
                 0,
                 PyorailyTormaysLuokittelu.EI_PYORAILUREITTI.toString()
-            )
+            ),
+            Luokittelutulos(
+                1,
+                LuokitteluType.RAITIOVAUNULIIKENNE,
+                0,
+                RaitiovaunuTormaysLuokittelu.EI_RAITIOVAUNULIIKENNETTA.toString()
+            ),
+            Luokittelutulos(1, LuokitteluType.BUSSILIIKENNE, 0, "")
         )
         val result = TormaystarkasteluCalculator().calculateAllIndeksit(TormaystarkasteluTulos("TUNNUS"), luokittelut)
         assertThat(result.pyorailyIndeksi).isEqualTo(1.0f)
@@ -31,19 +38,33 @@ internal class TormaystarkasteluCalculatorTest {
                 LuokitteluType.PYORAILYN_PAAREITTI,
                 4,
                 PyorailyTormaysLuokittelu.PAAREITTI.toString()
-            )
+            ),
+            Luokittelutulos(
+                1,
+                LuokitteluType.RAITIOVAUNULIIKENNE,
+                0,
+                RaitiovaunuTormaysLuokittelu.EI_RAITIOVAUNULIIKENNETTA.toString()
+            ),
+            Luokittelutulos(1, LuokitteluType.BUSSILIIKENNE, 0, "")
         )
         var result = TormaystarkasteluCalculator().calculateAllIndeksit(TormaystarkasteluTulos("TUNNUS"), luokittelut)
         assertThat(result.pyorailyIndeksi).isEqualTo(3.0f)
 
         // top case:
-       luokittelut = mutableListOf(
+        luokittelut = mutableListOf(
             Luokittelutulos(
                 1,
                 LuokitteluType.PYORAILYN_PAAREITTI,
                 5,
                 PyorailyTormaysLuokittelu.PRIORISOITU_REITTI.toString()
-            )
+            ),
+            Luokittelutulos(
+                1,
+                LuokitteluType.RAITIOVAUNULIIKENNE,
+                0,
+                RaitiovaunuTormaysLuokittelu.EI_RAITIOVAUNULIIKENNETTA.toString()
+            ),
+            Luokittelutulos(1, LuokitteluType.BUSSILIIKENNE, 0, "")
         )
         result = TormaystarkasteluCalculator().calculateAllIndeksit(TormaystarkasteluTulos("TUNNUS"), luokittelut)
         assertThat(result.pyorailyIndeksi).isEqualTo(3.0f)
@@ -65,11 +86,81 @@ internal class TormaystarkasteluCalculatorTest {
                 0,
                 RaitiovaunuTormaysLuokittelu.EI_RAITIOVAUNULIIKENNETTA.toString()
             ),
-            Luokittelutulos(1, LuokitteluType.BUSSILIIKENNE,0, "",   )
+            Luokittelutulos(1, LuokitteluType.BUSSILIIKENNE, 0, "")
         )
         val result = TormaystarkasteluCalculator().calculateAllIndeksit(TormaystarkasteluTulos("TUNNUS"), luokittelut)
         assertThat(result.pyorailyIndeksi).isEqualTo(1.0f)
         assertThat(result.joukkoliikenneIndeksi).isEqualTo(1.0f)
+    }
+
+
+    @Test
+    fun calculateAllIndeksit_whenIsNotOnRaitiovaunuButIsOnBusLine() {
+        //TODO: other classifications to be passed to not cause any errors?
+        val luokittelut = mutableListOf(
+            Luokittelutulos(
+                1,
+                LuokitteluType.PYORAILYN_PAAREITTI,
+                0,
+                PyorailyTormaysLuokittelu.EI_PYORAILUREITTI.toString()
+            ),
+            Luokittelutulos(
+                1,
+                LuokitteluType.RAITIOVAUNULIIKENNE,
+                0,
+                RaitiovaunuTormaysLuokittelu.EI_RAITIOVAUNULIIKENNETTA.toString()
+            ),
+            Luokittelutulos(1, LuokitteluType.BUSSILIIKENNE, 4, "")
+        )
+        val result = TormaystarkasteluCalculator().calculateAllIndeksit(TormaystarkasteluTulos("TUNNUS"), luokittelut)
+        assertThat(result.pyorailyIndeksi).isEqualTo(1.0f)
+        assertThat(result.joukkoliikenneIndeksi).isEqualTo(4.0f)
+    }
+
+    @Test
+    fun calculateAllIndeksit_whenIsOnRaitiovaunuAndIsLowerOnBusLine() {
+        //TODO: other classifications to be passed to not cause any errors?
+        val luokittelut = mutableListOf(
+            Luokittelutulos(
+                1,
+                LuokitteluType.PYORAILYN_PAAREITTI,
+                0,
+                PyorailyTormaysLuokittelu.EI_PYORAILUREITTI.toString()
+            ),
+            Luokittelutulos(
+                1,
+                LuokitteluType.RAITIOVAUNULIIKENNE,
+                4,
+                RaitiovaunuTormaysLuokittelu.OMA_KAISTA.toString()
+            ),
+            Luokittelutulos(1, LuokitteluType.BUSSILIIKENNE, 2, "")
+        )
+        val result = TormaystarkasteluCalculator().calculateAllIndeksit(TormaystarkasteluTulos("TUNNUS"), luokittelut)
+        assertThat(result.pyorailyIndeksi).isEqualTo(1.0f)
+        assertThat(result.joukkoliikenneIndeksi).isEqualTo(4.0f)
+    }
+
+    @Test
+    fun calculateAllIndeksit_whenBothRaitiovaunuAndBusLinesAreOnTopClass() {
+        //TODO: other classifications to be passed to not cause any errors?
+        val luokittelut = mutableListOf(
+            Luokittelutulos(
+                1,
+                LuokitteluType.PYORAILYN_PAAREITTI,
+                0,
+                PyorailyTormaysLuokittelu.EI_PYORAILUREITTI.toString()
+            ),
+            Luokittelutulos(
+                1,
+                LuokitteluType.RAITIOVAUNULIIKENNE,
+                5,
+                RaitiovaunuTormaysLuokittelu.JAETTU_KAISTA.toString()
+            ),
+            Luokittelutulos(1, LuokitteluType.BUSSILIIKENNE, 5, "")
+        )
+        val result = TormaystarkasteluCalculator().calculateAllIndeksit(TormaystarkasteluTulos("TUNNUS"), luokittelut)
+        assertThat(result.pyorailyIndeksi).isEqualTo(1.0f)
+        assertThat(result.joukkoliikenneIndeksi).isEqualTo(4.0f)
     }
 
 }
