5cec4806f627156ab2bb0c76866922a7a6b3cf3c
tintotti <tiina.m.poyhonen@gmail.com>
2021-02-26 09:03:39 +0200

    Added very simple integration test basis for creating new Tormaystarkastelu for hanke

--- services/hanke-service/src/integrationTest/kotlin/fi/hel/haitaton/hanke/tormaystarkastelu/TormaystarkasteluControllerITests.kt
+++ services/hanke-service/src/integrationTest/kotlin/fi/hel/haitaton/hanke/tormaystarkastelu/TormaystarkasteluControllerITests.kt
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -77,6 +78,57 @@ class TormaystarkasteluIControllerTests(@Autowired val mockMvc: MockMvc) {
                     .value(2.3f)
             )
         verify { laskentaService.getTormaystarkastelu(any()) }
+    }
+
+    @Test
+    fun `When createTormaystarkastelu is called for hanke without existing TormaystarkasteluTulos`() {
+
+        var hanke = Hanke(
+            123,
+            mockedHankeTunnus,
+            true,
+            "Hämeentien perusparannus ja katuvalot", "lorem ipsum dolor sit amet...",
+            ZonedDateTime.of(2020, 2, 20, 23, 45, 56, 0, TZ_UTC)
+                .truncatedTo(ChronoUnit.MILLIS),
+            ZonedDateTime.of(2030, 2, 20, 23, 45, 56, 0, TZ_UTC)
+                .truncatedTo(ChronoUnit.MILLIS),
+            Vaihe.OHJELMOINTI,
+            null,
+            1,
+            "Risto",
+            getCurrentTimeUTC(),
+            null,
+            null,
+            SaveType.DRAFT
+        )
+        // faking the service call
+        every { hankeService.loadHanke(any()) }.returns(hanke)
+
+        // faking the service call
+        val tormaystulos = TormaystarkasteluTulos(mockedHankeTunnus)
+        tormaystulos.perusIndeksi = 2.3f
+
+        //adding tormaystulos to hanke
+        hanke.tormaystarkasteluTulos = tormaystulos
+
+        every { laskentaService.calculateTormaystarkastelu(any()) }.returns(hanke)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/hankkeet/$mockedHankeTunnus/tormaystarkastelu")
+                .characterEncoding("UTF-8")
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.hankeTunnus")
+                    .value(mockedHankeTunnus)
+            ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.tormaystarkasteluTulos.perusIndeksi")
+                    .value(2.3f)
+            )
+        verify { laskentaService.calculateTormaystarkastelu(any()) }
     }
 
 }
