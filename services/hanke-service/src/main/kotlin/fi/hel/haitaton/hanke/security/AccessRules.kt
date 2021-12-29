536e0cb8170fa4382ae9a7c9f3a45c35a697713f
jampukka <janne.z.heikkila@gmail.com>
2021-12-29 10:22:43 +0200

    Add public-hankkeet endpoint

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/security/AccessRules.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/security/AccessRules.kt
@@ -15,6 +15,7 @@ object AccessRules {
         http.anonymous().and()
             .authorizeRequests()
             .mvcMatchers(HttpMethod.GET, "/organisaatiot").permitAll()
+            .mvcMatchers(HttpMethod.GET, "/public-hankkeet").permitAll()
             .mvcMatchers(HttpMethod.POST, "/hankkeet", "/hankkeet/**").hasRole("haitaton-user")
             .mvcMatchers(HttpMethod.GET, "/hankkeet", "/hankkeet/**").hasRole("haitaton-user")
             .mvcMatchers(HttpMethod.PUT, "/hankkeet/**").hasRole("haitaton-user")
