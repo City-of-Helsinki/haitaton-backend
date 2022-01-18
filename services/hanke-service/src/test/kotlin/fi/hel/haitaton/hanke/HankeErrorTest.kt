6f38f2f47197ea89fde56ed705618c0984e21e6d
jampukka <janne.z.heikkila@gmail.com>
2022-01-18 08:35:25 +0200

    Remove HankeErrorSerializer

---
+++ services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/HankeErrorTest.kt
@@ -0,0 +1,16 @@
+package fi.hel.haitaton.hanke
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class HankeErrorTest {
+
+    @Test
+    fun testJacksonSerialization() {
+        val err = HankeError.HAI0002
+        val expected = """{"errorMessage":"${err.errorMessage}","errorCode":"${err.errorCode}"}"""
+        assertThat(err.toJsonString()).isEqualTo(expected)
+    }
+
+}
