a7e0457c87b07c08574750c9d4a26bb6193cce17
jampukka <janne.z.heikkila@gmail.com>
2022-01-20 12:49:34 +0200

    Refactor TormaystarkasteluCalculator

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/tormaystarkastelu/TormaystarkasteluLaskentaServiceImpl.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/tormaystarkastelu/TormaystarkasteluLaskentaServiceImpl.kt
@@ -1,9 +1,6 @@
 package fi.hel.haitaton.hanke.tormaystarkastelu
 
-import fi.hel.haitaton.hanke.HankeNotFoundException
-import fi.hel.haitaton.hanke.HankeService
 import fi.hel.haitaton.hanke.domain.Hanke
-import fi.hel.haitaton.hanke.geometria.HankeGeometriatService
 import org.springframework.beans.factory.annotation.Autowired
 
 open class TormaystarkasteluLaskentaServiceImpl(
@@ -29,37 +26,7 @@ open class TormaystarkasteluLaskentaServiceImpl(
         val rajaArvot = LuokitteluRajaArvot()
 
         val luokittelutulos = luokitteluService.calculateTormaystarkasteluLuokitteluTulos(hanke, rajaArvot)
-        val laskentatulos = TormaystarkasteluCalculator.calculateAllIndeksit(initTormaystarkasteluTulos(hanke), luokittelutulos)
-
-        laskentatulos.tila = TormaystarkasteluTulosTila.VOIMASSA
-
-        return laskentatulos
-    }
-
-    private fun initTormaystarkasteluTulos(hanke: Hanke): TormaystarkasteluTulos {
-        if (hanke.hankeTunnus == null) {
-            throw IllegalArgumentException("hankeTunnus non existent when trying to get TormaysTarkastelu")
-        }
-
-        val laskentatulos = TormaystarkasteluTulos(hanke.hankeTunnus!!)
-        laskentatulos.hankeId = hanke.id!!
-        laskentatulos.hankeGeometriatId = hanke.geometriat!!.id!!
-        // TODO: in the future this would contain "Viereiset hankkeet" too
-
-        return laskentatulos
-    }
-
-    private fun copyTormaystarkasteluTulosFromEntity(
-        tttEntity: TormaystarkasteluTulosEntity,
-        ttt: TormaystarkasteluTulos
-    ) {
-        ttt.liikennehaittaIndeksi = tttEntity.liikennehaitta?.copy()
-        ttt.perusIndeksi = tttEntity.perus
-        ttt.pyorailyIndeksi = tttEntity.pyoraily
-        ttt.joukkoliikenneIndeksi = tttEntity.joukkoliikenne
-        ttt.tila = tttEntity.tila
-        // TODO? domain object does not (yet?) have createdAt or tilaChangedAt fields?
-        //  But are they even needed/used there (yet?)
+        return TormaystarkasteluCalculator.calculateAllIndeksit(luokittelutulos)
     }
 
 }
