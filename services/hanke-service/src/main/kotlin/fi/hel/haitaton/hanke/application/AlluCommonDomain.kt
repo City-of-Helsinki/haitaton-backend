5e1f83dc83b73f86f21a4caa3dedbbd7e589715a
jampukka <janne.z.heikkila@gmail.com>
2022-02-21 08:47:53 +0200

    Change optional fields to nullable

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/application/AlluCommonDomain.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/application/AlluCommonDomain.kt
@@ -3,24 +3,24 @@ package fi.hel.haitaton.hanke.application
 data class CustomerWithContacts(val customer: Customer, val contacts: List<Contact>)
 
 data class Contact(
-        val name: String,
-        val postalAddress: PostalAddress,
-        val email: String,
-        val phone: String,
+        val name: String?,
+        val postalAddress: PostalAddress?,
+        val email: String?,
+        val phone: String?,
         val orderer: Boolean = false
 )
 
 data class Customer(
         val type: CustomerType,
         val name: String,
-        val postalAddress: PostalAddress,
-        val email: String,
-        val phone: String,
-        val registryKey: String, // ssn or y-tunnus
-        val ovt: String, // e-invoice identifier (ovt-tunnus)
-        val invoicingOperator: String, // e-invoicing operator code
         val country: String, // ISO 3166-1 alpha-2 country code
-        val sapCustomerNumber: String // customer's sap number
+        val postalAddress: PostalAddress?,
+        val email: String?,
+        val phone: String?,
+        val registryKey: String?, // ssn or y-tunnus
+        val ovt: String?, // e-invoice identifier (ovt-tunnus)
+        val invoicingOperator: String?, // e-invoicing operator code
+        val sapCustomerNumber: String? // customer's sap number
 )
 
 data class PostalAddress(val streetAddress: StreetAddress, val postalCode: String, val city: String)
