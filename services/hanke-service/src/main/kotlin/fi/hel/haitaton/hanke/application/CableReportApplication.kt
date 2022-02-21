8c00f64715e0f16e8c8b27769c233e7d8f075acd
jampukka <janne.z.heikkila@gmail.com>
2022-02-21 08:45:55 +0200

    Minor touches to CableReportService, dont add JTS just for optional area calculation

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/application/CableReportApplication.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/application/CableReportApplication.kt
@@ -1,27 +1,39 @@
 package fi.hel.haitaton.hanke.application
 
-import org.locationtech.jts.geom.GeometryCollection
+import org.geojson.GeometryCollection
 import java.time.ZonedDateTime
 
 class CableReportApplication(
-        name: String,
-        customerWithContacts: CustomerWithContacts,
-        geometry: GeometryCollection,
-        startTime: ZonedDateTime,
-        endTime: ZonedDateTime,
-        pendingOnClient: Boolean,
-        identificationNumber: String,
-        clientApplicationKind: String,
-        workDescription: String,
-        contractorWithContacts: CustomerWithContacts, // työn suorittaja
+        // Common, required
+        val name: String,
+        val customerWithContacts: CustomerWithContacts,
+        val geometry: GeometryCollection,
+        val startTime: ZonedDateTime,
+        val endTime: ZonedDateTime,
+        var pendingOnClient: Boolean,
+        val identificationNumber: String,
+
+        // CableReport specific, required
+        val clientApplicationKind: String,
+        val workDescription: String,
+        val contractorWithContacts: CustomerWithContacts, // työn suorittaja
 ) {
+    // Common, not required
+    var postalAddress: PostalAddress? = null
     var representativeWithContacts: CustomerWithContacts? = null
     var invoicingCustomer: Customer? = null
     var customerReference: String? = null
+    var area: Double? = null
+
+    // CableReport specific, not required
     var propertyDeveloperWithContacts: CustomerWithContacts? = null // rakennuttaja
     var constructionWork: Boolean = false
     var maintenanceWork: Boolean = false
     var emergencyWork: Boolean = false
     var propertyConnectivity: Boolean = false // tontti-/kiinteistöliitos
-    val area: Double by lazy { geometry.area }
 }
+
+data class CableReportInformationRequestResponse(
+        val applicationData: CableReportApplication,
+        val updatedFields: List<InformationRequestFieldKey>
+)
--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/application/CableReportService.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/application/CableReportService.kt
@@ -4,15 +4,13 @@ import java.time.ZonedDateTime
 
 interface CableReportService {
 
-    fun getCurrentStatus(applicationId: Int): ApplicationStatus
+    fun getCurrentStatus(applicationId: Int): ApplicationStatus?
     fun getApplicationStatusEvents(applicationId: Int, eventsAfter: ZonedDateTime?): List<ApplicationStatusEvent>
 
     fun create(cableReport: CableReportApplication): Int
-    fun update(applicationId: Int, cableReport: CableReportApplication): Int
+    fun update(applicationId: Int, cableReport: CableReportApplication)
 
     fun addAttachment(applicationId: Int, metadata: AttachmentInfo, file: ByteArray)
-    fun getAttachments(applicationId: Int): List<AttachmentInfo>
-    fun getAttachment(applicationId: Int, attachmentId: Int): ByteArray
 
     fun getInformationRequests(applicationId: Int): List<InformationRequest>
     fun respondToInformationRequest(
@@ -23,5 +21,7 @@ interface CableReportService {
     )
 
     fun getDecisionPDF(applicationId: Int): ByteArray
+    fun getDecisionAttachments(applicationId: Int): List<AttachmentInfo>
+    fun getDecisionAttachmentData(applicationId: Int, attachmentId: Int): ByteArray
 
 }
