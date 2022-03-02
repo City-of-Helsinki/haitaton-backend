d57121552a46907fbf1f90aff66d874bbbf7af46
jampukka <janne.z.heikkila@gmail.com>
2022-03-02 11:53:14 +0200

    Rename Application to AlluApplicaton, add ApplicationDTO, Optional<> to Nullable types, let Controller methods to return the object, not ResponseEntities

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/application/ApplicationService.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/application/ApplicationService.kt
@@ -1,42 +1,66 @@
 package fi.hel.haitaton.hanke.application
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.treeToValue
+import fi.hel.haitaton.hanke.OBJECT_MAPPER
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Repository
-import java.util.*
 
 class ApplicationService(private val repo : ApplicationRepository) {
-    fun create(application: Application) : Application {
-        application.userId = SecurityContextHolder.getContext().authentication.name
-        return repo.save(application);
-    }
-
-    fun getApplicationById(id: Long) : Optional<Application> {
+    fun create(application: ApplicationDTO) : ApplicationDTO {
         val userId = SecurityContextHolder.getContext().authentication.name
-        return repo.findOneByIdAndUserId(id,userId)
+        val alluApplication = repo.save(AlluApplication(
+                id = null,
+                userId = userId,
+                applicationType = application.applicationType,
+                applicationData = application.applicationData
+        ))
+        return applicationToDto(alluApplication)
     }
 
-    fun getAllApplicationsForCurrentUser() : List<Application>{
+    fun getApplicationById(id: Long) : ApplicationDTO? {
         val userId = SecurityContextHolder.getContext().authentication.name
-        return repo.getAllByUserId(userId)
+        return repo.findOneByIdAndUserId(id, userId)?.let { applicationToDto(it) }
     }
 
-    fun updateApplicationData(id: Long, newApplicationData: JsonNode): Optional<Application> {
+    fun getAllApplicationsForCurrentUser() : List<ApplicationDTO> {
+        val userId = SecurityContextHolder.getContext().authentication.name
+        return repo.getAllByUserId(userId).map { applicationToDto(it) }
+    }
+
+    fun updateApplicationData(id: Long, newApplicationData: JsonNode): ApplicationDTO? {
         val currentUser = SecurityContextHolder.getContext().authentication.name
-        val found = repo.findOneByIdAndUserId(id, currentUser)
-        if(found.isEmpty)
-            return found
-
-        val applicationToUpdate = found.get()
+        val applicationToUpdate = repo.findOneByIdAndUserId(id, currentUser) ?: return null
         applicationToUpdate.applicationData = newApplicationData
-
-        return Optional.of(repo.save(applicationToUpdate))
+        return applicationToDto(repo.save(applicationToUpdate))
     }
+
+    /**
+     * @return desired http status code. could also throw exceptions or create some enum for return type
+     */
+    fun sendApplication(id: Long): Int {
+        val currentUser = SecurityContextHolder.getContext().authentication.name
+        val application = repo.findOneByIdAndUserId(id, currentUser) ?: return 404
+
+        // TODO: Make sure we haven't sent the application yet
+
+        when (application.applicationType) {
+            ApplicationType.CABLE_REPORT -> {
+                val cableReportApplication: CableReportApplication =
+                        OBJECT_MAPPER.treeToValue(application.applicationData) ?: return 400
+                // TODO: Try to send the application to ALLU
+            }
+        }
+
+        // TODO: Mark application as sent, disallow further updates on the application
+        return 200
+    }
+
 }
 
 @Repository
-interface ApplicationRepository : JpaRepository<Application, Long> {
-    fun findOneByIdAndUserId(id: Long, userId: String) : Optional<Application>
-    fun getAllByUserId(userId: String): List<Application>
+interface ApplicationRepository : JpaRepository<AlluApplication, Long> {
+    fun findOneByIdAndUserId(id: Long, userId: String) : AlluApplication?
+    fun getAllByUserId(userId: String): List<AlluApplication>
 }
