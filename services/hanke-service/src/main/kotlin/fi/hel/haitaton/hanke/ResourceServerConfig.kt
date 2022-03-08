e9de798a1a7a1fd360f9093ae25829549d04911a
jampukka <janne.z.heikkila@gmail.com>
2022-03-08 16:29:16 +0200

    Use tunnistamo, disable auth-service and auth-db

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/ResourceServerConfig.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/ResourceServerConfig.kt
@@ -1,48 +1,24 @@
 package fi.hel.haitaton.hanke
 
 import fi.hel.haitaton.hanke.security.AccessRules
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.access.expression.SecurityExpressionHandler
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
-import org.springframework.security.oauth2.provider.expression.OAuth2WebSecurityExpressionHandler
-import org.springframework.security.oauth2.provider.token.TokenStore
-import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore
-import org.springframework.security.web.FilterInvocation
 
 @Configuration
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 class ResourceServerConfig : ResourceServerConfigurerAdapter() {
 
-    @Value("\${claim.aud}")
-    lateinit var claimAud: String
-
-    @Value("\${jwkSetUri}")
-    lateinit var urlJwk: String
-
     override fun configure(http: HttpSecurity) {
         AccessRules.configureHttpAccessRules(http)
     }
 
-    override fun configure(resources: ResourceServerSecurityConfigurer) {
-        resources.tokenStore(tokenStore())
-        resources.resourceId(claimAud)
-        resources.expressionHandler(handler())
-    }
-
     @Bean
-    fun tokenStore(): TokenStore = JwkTokenStore(urlJwk)
+    fun principalExtractor(): PrincipalExtractor = PrincipalExtractor { it["sub"] }
 
-    @Bean
-    fun securityEvaluationContextExtension(): SecurityEvaluationContextExtension = SecurityEvaluationContextExtension()
-
-    @Bean
-    fun handler(): SecurityExpressionHandler<FilterInvocation> = OAuth2WebSecurityExpressionHandler()
 }
