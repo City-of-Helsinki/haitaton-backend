75c6bbbd61b684ac8056ee3575ca4d76ce33160f
Esa Östring <esa.ostring@ubigu.fi>
2022-09-09 13:21:00 +0300

    GIS data fetch via dockerized scripts started
    * fetch data, skeleton implementation
    * configuration skeleton
    * container creation for fetch
    * initial container creation for database and processing

---
+++ scripts/gis-material-update/fetch/fetch_data.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+config_yaml="/config.yaml"
+
+# Parse configuration YAML
+# Built using:
+# yq --version
+# yq (https://github.com/mikefarah/yq/) version 4.16.2
+
+data_object=$1
+
+parse_config () {
+    parsed_value=$(yq eval -e ".${1}" - < $config_yaml) > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        retval="$parsed_value"
+    else
+        retval=""
+    fi
+    echo "$retval"
+}
+
+# dynamic configuration
+# refer to configuration YAML
+
+cfg_addr () {
+    echo "${data_object}.addr"
+}
+
+cfg_local_file () {
+    echo "${data_object}.local_file"
+}
+
+cfg_layer () {
+    echo "${data_object}.layer"
+}
+
+# if we have progressed this far, object is supported in configuration
+# now it remains to find out how to download files
+addr=$(parse_config $(cfg_addr $data_object))
+layer=$(parse_config $(cfg_layer $data_object))
+local_file=$(parse_config $(cfg_local_file $data_object))
+
+case $data_object in
+hsl|osm)
+    wget -O "$local_file" "$addr"
+    ;;
+ylre_katualue|ylre_katuosat|maka_autoliikennemaarat)
+    ogr2ogr -f GPKG "$local_file" WFS:"$addr" "$layer"
+    ;;
+*)
+    echo "Not supported"
+    return 1
+    ;;
+esac
+# https://stackoverflow.com/questions/22049212/docker-copying-files-from-docker-container-to-host
