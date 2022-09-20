f2cf39cb71a7f929bcbe4469d1d5985339a22b5e
Esa Östring <esa.ostring@ubigu.fi>
2022-09-20 08:47:40 +0300

    Documentation
    * clarified instructions for setup
    * database port to environment variable

--- scripts/gis-material-update/inspect-disk.sh
+++ scripts/gis-material-update/inspect-disk.sh
@@ -1,4 +1,9 @@
-docker container create --name dummy -v haitaton_gis_prepare:/haitaton-gis ubuntu
+#!/bin/sh
+
+docker container create --name dummy \
+    -v haitaton_gis_prepare:/haitaton-gis \
+    -v $(pwd)/haitaton-downloads:/downloads \
+    ubuntu
 
 docker container run --rm -it --volumes-from dummy ubuntu /bin/bash
 
