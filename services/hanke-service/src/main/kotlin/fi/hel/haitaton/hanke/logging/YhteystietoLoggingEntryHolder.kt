64220f8662d66ad5883118929570a92e68ab3797
Topias Heinonen <topias.heinonen@gofore.com>
2022-09-20 10:26:06 +0300

    HAI-1222 Add ktfmt for automated formatting (#204)
    Use the Spotless Gradle plugin to run ktfmt on the codebase. Ktfmt is a
    opinionated formatter for Kotlin code that's non-customizable by design.
    
    Spotless checks during the build stage that all code is formatted with
    ktfmt. If the code is not formatted correctly, the build will fail.
    
    Only code changed since the origin/dev will be checked and formatted.
    When a file is touched, the whole file needs to be reformatted, though.
    
    The formatting can be checked with:
    ```
    ./gradlew spotlessCheck
    ```
    And the code can be reformatted with:
    ```
    ./gradlew :services:hanke-service:spotlessApply
    ```
    
    The formatting broke the line wrapping on some comments, so I fixed them
    and rewrote some comments to make them shorter and/or more informative.

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/logging/YhteystietoLoggingEntryHolder.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/logging/YhteystietoLoggingEntryHolder.kt
@@ -8,9 +8,8 @@ import org.springframework.web.context.request.ServletRequestAttributes
 const val PERSONAL_DATA_LOGGING_MAX_IP_LENGTH = 40
 
 /**
- * Used to hold the map of yhteystieto entities to logging entries during processing,
- * so that new ids of new yhteystietos can be applied after they have been saved and
- * thus received those ids.
+ * Used to hold the map of yhteystieto entities to logging entries during processing, so that new
+ * ids of new yhteystietos can be applied after they have been saved and thus received those ids.
  *
  * Basic use:
  * 1. Create instance of this class.
@@ -29,37 +28,42 @@ class YhteystietoLoggingEntryHolder {
     // Holds the ids of Yhteystietos that were in the Hanke before this request handling.
     private val previousYhteystietoIds: HashSet<Int> = hashSetOf()
 
-    fun hasEntries() : Boolean = auditLogEntries.isNotEmpty()
+    fun hasEntries(): Boolean = auditLogEntries.isNotEmpty()
 
-    fun idList() : String {
+    fun idList(): String {
         return auditLogEntries.map { it.id }.joinToString()
     }
 
     fun initWithOldYhteystietos(oldYTs: MutableList<HankeYhteystietoEntity>) {
         oldYTs.forEach {
             val yhteystietoId =
-                it.id ?: throw DatabaseStateException("A persisted HankeYhteystietoEntity somehow missing id")
+                it.id
+                    ?: throw DatabaseStateException(
+                        "A persisted HankeYhteystietoEntity somehow missing id"
+                    )
             previousYhteystietoIds.add(yhteystietoId)
         }
     }
 
     /**
-     * Creates audit log entries for the given action to this holder. This
-     * function will not save them, or set IP to them; those must be done
-     * separately before returning from the relevant process.
+     * Creates audit log entries for the given action to this holder. This function will not save
+     * them, or set IP to them; those must be done separately before returning from the relevant
+     * process.
      *
-     * The id of the yhteystieto will be taken from the oldEntity, unless it or
-     * its id is null and the newEntity is non-null, in which case it is taken
-     * from the newEntity. This rule handles all cases of create, update, and
-     * delete, _if_ this function is called after saving the entities for create
-     * action(s).
+     * The id of the yhteystieto will be taken from the oldEntity, unless it or its id is null and
+     * the newEntity is non-null, in which case it is taken from the newEntity. This rule handles
+     * all cases of create, update, and delete, _if_ this function is called after saving the
+     * entities for create action(s).
+     *
+     * For the old entity value, make a clone/copy of the persisted entity before making changes to
+     * it, if necessary.
      *
      * @param action can not be null
      * @param failed can not be null
      * @param failureDescription description for the failure, if one is available
-     * @param oldEntity for logging the previous field values (make a clone/copy before making changes
-     *              to the persisted entity, if necessary); can be null (when creating new)
-     * @param newEntity for logging the new state of field values; can be null (when deleting or reading)
+     * @param oldEntity for logging the previous field values; can be null (when creating new)
+     * @param newEntity for logging the new state of field values; can be null (when deleting or
+     * reading)
      * @param userId ID of the user making the API call
      */
     fun addLogEntriesForEvent(
@@ -73,26 +77,30 @@ class YhteystietoLoggingEntryHolder {
         // Note, use oldEntity delete and update and newEntity for create-action.
         val yhteystietoId = oldEntity?.id ?: newEntity?.id
 
-        val auditLogEntry = AuditLogEntry(
-            userId = userId,
-            action = action,
-            status = if(failed) Status.FAILED else Status.SUCCESS,
-            failureDescription = failureDescription,
-            objectType = ObjectType.YHTEYSTIETO,
-            objectId = yhteystietoId,
-            objectBefore = oldEntity?.toChangeLogJsonString(),
-            objectAfter = newEntity?.toChangeLogJsonString(),
-        )
+        val auditLogEntry =
+            AuditLogEntry(
+                userId = userId,
+                action = action,
+                status = if (failed) Status.FAILED else Status.SUCCESS,
+                failureDescription = failureDescription,
+                objectType = ObjectType.YHTEYSTIETO,
+                objectId = yhteystietoId,
+                objectBefore = oldEntity?.toChangeLogJsonString(),
+                objectAfter = newEntity?.toChangeLogJsonString(),
+            )
         auditLogEntries.add(auditLogEntry)
     }
 
     /**
-     * Goes through the entries that were new (have null yhteystietoId in the log entry,
-     * but non-null in the entity), and copies the new ids into the corresponding log entries.
+     * Goes through the entries that were new (have null yhteystietoId in the log entry, but
+     * non-null in the entity), and copies the new ids into the corresponding log entries.
      */
-    fun addLogEntriesForNewYhteystietos(savedHankeYhteystietoEntities: MutableList<HankeYhteystietoEntity>, userid: String) {
+    fun addLogEntriesForNewYhteystietos(
+        savedHankeYhteystietoEntities: MutableList<HankeYhteystietoEntity>,
+        userid: String
+    ) {
         // Go through the saved yhteystietos, filter for processing those that didn't exist
-        // before, and add log entries for them now, as their id's are now known.
+        // before, and add log entries for them now, as their IDs are now known.
         // (Obviously, they all succeeded, since they have been saved already.)
         savedHankeYhteystietoEntities
             .filter { !previousYhteystietoIds.contains(it.id) }
@@ -107,19 +115,24 @@ class YhteystietoLoggingEntryHolder {
     }
 
     /**
-     * If request context (attributes) exists, gets the IP from it and applies
-     * to all the log entries currently held in this holder.
+     * If request context (attributes) exists, gets the IP from it and applies to all the log
+     * entries currently held in this holder.
+     *
      * NOTE: very very very simplified implementation. Needs a lot of improvement.
      */
     fun applyIpAddresses() {
-        val attribs = (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes?) ?: return
+        val attribs =
+            (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes?) ?: return
         val request = attribs.request
         // Very simplified version for now.
-        // For starters, see e.g. https://stackoverflow.com/questions/22877350/how-to-extract-ip-address-in-spring-mvc-controller-get-call
-        // Combine all the various ideas into one, and note that even then it is not even half-way to
-        // proper solution. Hopefully one can find a ready-made fully thought out implementation.
+        // For starters, see e.g.
+        // https://stackoverflow.com/questions/22877350/how-to-extract-ip-address-in-spring-mvc-controller-get-call
+        // Combine all the various ideas into one, and note that even then it is not even half-way
+        // to a proper solution. Hopefully one can find a ready-made fully thought out
+        // implementation.
         var ip: String = request.getHeader("X-FORWARDED-FOR") ?: request.remoteAddr ?: return
-        // Just to make sure it won't break the db if someone put something silly long in the header:
+        // Just to make sure it won't break the db if someone put something silly long in the
+        // header:
         if (ip.length > PERSONAL_DATA_LOGGING_MAX_IP_LENGTH)
             ip = ip.substring(0, PERSONAL_DATA_LOGGING_MAX_IP_LENGTH)
 
