523142f9eb0ad67116c0797420665274ae36b600
Esa Östring <esaostring@users.noreply.github.com>
2022-11-09 11:08:54 +0200

    HAI 1176/Process HSL GTFS material to tormays polygons (#219)
    * Started writing GTFS parsing
    
    * HSL buses GIS material processing
    
    * compute GIS material based on HSL GTFS
    * fetch geographic boundary for city of Helsinki
    
    * Support tormays-data output
    
    * fixed point order in linestring generation
    * file output handling enhanced
    
    * Refactor bus line computation
    
    * deliver run time environment via env variable
    * support multiple database connection configurations
    
    * Added dockerignore
    
    * ignore pycache directories
    * comment added in config module
    
    * Deployment profile first use
    
    * directory name handling according to deployment profile
    
    * Python formatters incorporated
    * Using black and isort according to coding standard
    
    * Visual edits
    
    * gitignore updated
    * config-module visual edits
    
    * Clening unused code
    
    * Cleanups
    
    * variable name clarifications
    * implemented tests
    * enhanced documentation
    
    * HSL GTFS is broken.
    
    * Added tests
    
    * Added file check to beginning of processing
    
    * Helsinki city polygon file check added to beginning of processing
    * fixed test case
    
    * Instructions updated
    
    * More tests written
    
    * tests for rush hour time generation
    * Minor documentation cleanup
    
    * Clarified variable naming
    
    * Classmethod parameter renamed
    
    * Review comment corrections
    
    * made configuration file structure clearer
    * refactored rush hour clarifications

--- scripts/gis-material-update/process/process_data.py
+++ scripts/gis-material-update/process/process_data.py
@@ -1,28 +1,47 @@
+"""Main entrypoint script for material processing.
+"""
+import os
 import sys
 
 from modules.config import Config
 from modules.gis_processing import GisProcessor
 from modules.hsl import HslBuses
 
-def process_item(item : str):
+
+def process_item(item: str, cfg: Config):
     print(f"Processing item: {item}")
-    gis_processor = instantiate_processor(item)
+    gis_processor = instantiate_processor(item, cfg)
     gis_processor.process()
     gis_processor.persist_to_database()
     gis_processor.save_to_file()
     pass
 
-def instantiate_processor(item : str) -> GisProcessor:
+
+def instantiate_processor(item: str, cfg: Config) -> GisProcessor:
     """Instantiate correct class for processing data."""
     if item == "hsl":
-        return HslBuses()
+        return HslBuses(cfg, validate_gtfs=False)
     else:
-        raise Exception(f"Configuration not recognized: {item}")
+        try:
+            raise RuntimeError("Configuration not recognized: {}".format(item))
+        except Exception as e:
+            print("{}".format(e))
+
 
 if __name__ == "__main__":
-    cfg = Config()
 
-    print("Processing data:")
+    deployment_profile = os.environ.get("TORMAYS_DEPLOYMENT_PROFILE")
+
+    if deployment_profile in ["local_docker_development", "local_development"]:
+        use_deployment_profile = deployment_profile
+    else:
+        raise ValueError("Deployment profile is not detected")
+
+    print("Using deployment profile: '{}'".format(use_deployment_profile))
+
+    cfg = Config().with_deployment_profile(use_deployment_profile)
+
+    print("Processing data.")
 
     for item in sys.argv[1:]:
-        process_item(item)
+        process_item(item, cfg)
