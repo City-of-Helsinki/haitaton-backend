f5b318e00cfd39a16c5e015871c06c5c2076655d
Esa Östring <esaostring@users.noreply.github.com>
2022-12-15 11:08:48 +0200

    HAI-1331/update split to download and database update (#236)
    * Started separating download and sql insert
    
    * Import script updated
    
    * detect potentially missing file
    
    * Comment fixes

---
+++ scripts/hel-gis-data-download.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Script to download tormays polygons GIS material from project fileshare
+# Maintenance VPN is needed to access the fileshare.
+
+set -e
+
+# Kudos: https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
+(return 0 2>/dev/null) && sourced=1 || sourced=0
+
+if [ "$sourced" -eq "1" ] || ! test -z "$(type -p)" ; then
+    echo "Use bash to run script."
+    return 0
+fi
+
+# Common variables are fetched from separate file
+. ./hel-gis-data-variables.sh
+
+FILESHARE_ADDR="http://haitaton-fileshare-dev.internal.apps.arodevtest.hel.fi"
+
+if ! [ -d "$POLYS_DIR" ]; then
+    mkdir "$POLYS_DIR"
+fi
+
+start=`date +%s`
+
+counter=0
+for file in ${FILES[@]}; do
+    counter=$((counter + 1))
+    echo "[$counter/${#FILES[@]}] Downloading ${file} to $POLYS_DIR"
+    wget --show-progress "${FILESHARE_ADDR}/${file}" -O "${POLYS_DIR}/${file}"
+    echo "Downloaded ${file}"
+done
+
+end=`date +%s`
+runtime=$((end-start))
+rt=$(echo "scale=2; $runtime / 60" | bc -l)
+echo "Download took $rt minutes"
+echo "Files are downloaded to directory: ${POLYS_DIR}"
