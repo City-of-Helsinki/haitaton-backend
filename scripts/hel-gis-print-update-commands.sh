eab455d4745fbee5645bf6d84a8da75b6257cc30
Esa Östring <esa.ostring@ubigu.fi>
2023-01-27 11:28:30 +0200

    Helper scripts added

--- scripts/hel-gis-print-update-commands.sh
+++ scripts/hel-gis-print-update-commands.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# Script to download tormays polygons GIS material from project fileshare
-# Maintenance VPN is needed to access the fileshare.
-
-set -e
+# Print update commands to stdout
 
 config_yaml="./hel-gis-data-variables.yaml"
 
@@ -38,37 +35,18 @@ if [ "$FILESHARE" = "" ]; then
     exit 3
 fi
 
-POLYS_DIR=$(parse_config polys_dir)
-
-if ! [ -d "$POLYS_DIR" ]; then
-    echo "Directory '$POLYS_DIR' does not exist, creating."
-    mkdir "$POLYS_DIR"
-fi
-
 # All tormays-polygon files
 FILES=$(parse_config "files")
-
-if [ "$FILES" = "" ]; then
-    echo "Files not found from configuration file: '$config_yaml'"
-    exit 3
-fi
 
 # file count for loop reference
 # This approach does not support file names with spaces
 file_count=$(echo $FILES | wc -w)
 
-start=`date +%s`
-
 counter=0
-for file in $FILES; do
+for file in ${FILES}; do
     counter=$((counter + 1))
-    echo "[$counter/$file_count] Downloading ${file} to $POLYS_DIR"
-    wget --show-progress "${FILESHARE}/${file}" -O "${POLYS_DIR}/${file}"
-    echo "Downloaded ${file}"
+    layer=${file%.gpkg}
+    echo "# [$counter/$file_count] Updating: $trg for file: '$file', layer: '$layer'"
+    echo 'ogr2ogr -overwrite -f PostgreSQL PG:"host=$HAITATON_HOST dbname=$HAITATON_DATABASE user=$HAITATON_USER password=$HAITATON_PASSWORD sslmode=require" /vsicurl/'$FILESHARE/$file $layer
+    echo ""
 done
-
-end=`date +%s`
-runtime=$((end-start))
-rt=$(echo "scale=2; $runtime / 60" | bc -l)
-echo "Download took $rt minutes"
-echo "Files are downloaded to directory: ${POLYS_DIR}"
---
+++ scripts/hel-gis-print-upload-commands.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Print upload commands to stdout
+
+# common configuration
+config_yaml="./hel-gis-data-variables.yaml"
+
+# source directory name
+SOURCEDIR=gis-material-update/haitaton-gis-output
+
+usage () {
+    echo "Usage: $0"
+    exit 2
+}
+
+. ./parse_config_common.sh
+
+if [ $# -ne 0 ]; then
+    usage
+fi
+
+# check oc status
+# User should be logged in to find out pod name
+if ! $(oc whoami > /dev/null 2>&1 ); then
+    echo "oc: not logged in"
+    exit 3
+fi
+
+# All tormays-polygon files
+FILES=$(parse_config "files")
+
+# file share address pod name
+FILESHARE_NAME=$(oc get pods | grep fileshare | grep Running | head -1 | awk '{print $1}')
+
+# file count for loop reference
+# This approach does not support file names with spaces
+file_count=$(echo $FILES | wc -w)
+
+counter=0
+for file in $FILES; do
+    counter=$((counter + 1))
+    layer=${file%.gpkg}
+    if [ -f ${SOURCEDIR}/$file ]; then
+        echo "# [$counter/$file_count] Uploading: '$file', layer: '$layer'"
+        echo "oc cp ${SOURCEDIR}/$file ${FILESHARE_NAME}:/var/www/html/public"
+        echo ""
+    else
+        echo "# [$counter/$file_count] Not uploading: (processed file does not exist: '$file')"
+    fi
+done
