94de466149cfdbbd812dccc9a49ffc6ca9e32ef8
Esa Östring <esa.ostring@ubigu.fi>
2023-01-27 13:38:26 +0200

    bash shell is not required anymore

--- scripts/hel-gis-data-import-to-db.sh
+++ scripts/hel-gis-data-import-to-db.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 # Script to import downloaded GIS material to local development database
 
 set -e
 
-# Kudos: https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
-(return 0 2>/dev/null) && sourced=1 || sourced=0
+config_yaml="./hel-gis-data-variables.yaml"
 
-if [ "$sourced" -eq "1" ] || ! test -z "$(type -p)" ; then
-    echo "Use bash to run script."
-    return 0
+. ./parse_config_common.sh
+
+usage () {
+    echo "Usage: $0 <source directory>"
+    exit 2
+}
+
+if [ $# -ne 1 ] || ! [ -d "$1" ]; then
+    echo "Source directory must exist"
+    usage
+else
+    source_directory=$1
 fi
-
-# Common variables are fetched from separate file
-. ./hel-gis-data-variables.sh
 
 DB_NAME="haitaton"
 DB_USER="haitaton_user"
@@ -21,19 +26,33 @@ DB_PASS="haitaton"
 DB_HOST="db"
 DB_PORT="5432"
 
+# All tormays-polygon files
+FILES=$(parse_config "files")
+
+if [ "$FILES" = "" ]; then
+    echo "Files not found from configuration file: '$config_yaml'"
+    exit 3
+fi
+
+# file count for loop reference
+# This approach does not support file names with spaces
+file_count=$(echo $FILES | wc -w)
+
 start=`date +%s`
 
 counter=0
-for file in ${FILES[@]}; do
+for file in $FILES; do
     counter=$((counter + 1))
-    if [ -f "${POLYS_DIR}/${file}" ]; then
-        echo "[$counter/${#FILES[@]}] Importing '${file}'"
-        docker run --rm --network=haitaton-backend_backbone -v $(pwd)/tormays_polys:/tormays_polys osgeo/gdal:alpine-small-latest \
-            ogr2ogr -overwrite -f PostgreSQL PG:"dbname='$DB_NAME' host='$DB_HOST' port='$DB_PORT' user='$DB_USER' password='$DB_PASS'" \
-            ${POLYS_DIR}/${file}
+    if [ -f "${source_directory}/${file}" ]; then
+        echo "[$counter/${file_count}] Importing '${file}'"
+        docker run --rm --network=haitaton-backend_backbone \
+            -v $(pwd)/tormays_polys:/tormays_polys osgeo/gdal:alpine-small-latest \
+            ogr2ogr -overwrite -f PostgreSQL \
+                PG:"dbname='$DB_NAME' host='$DB_HOST' port='$DB_PORT' user='$DB_USER' password='$DB_PASS'" \
+                ${source_directory}/${file}
         echo "Imported '${file}'"
     else
-        echo "[$counter/${#FILES[@]}] Skipping, '${file}' does not exist."
+        echo "[$counter/${file_count}] Skipping, '${source_directory}/${file}' does not exist."
     fi
 done
 
