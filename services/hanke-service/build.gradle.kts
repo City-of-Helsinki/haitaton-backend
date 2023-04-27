2f4b6e087fb9403bb5907ef4903f8a98071b61a1
Niko Pitkonen <78962935+pitkni@users.noreply.github.com>
2023-04-27 12:13:21 +0300

    HAI-1274 Fix multipart request param bug (#309)
    HAI-1274 Fix file upload issue
    
    After upgrading spring boot version, attachment uploads seem to fail.
    
    Problem seems to happen due to multipartresolver bean from apache. Spring Boot should work with multipart files without any additional configuration beans.
    
    Removing the mentioned bean seems to solve the upload issue at least locally.

--- services/hanke-service/build.gradle.kts
+++ services/hanke-service/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
 	implementation("org.liquibase:liquibase-core")
 	implementation("com.github.blagerweij:liquibase-sessionlock:1.4.0")
 	implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
-	implementation("commons-fileupload:commons-fileupload:1.5")
+	implementation("commons-io:commons-io:2.11.0")
 	implementation("com.github.librepdf:openpdf:1.3.30")
 	implementation("net.pwall.mustache:kotlin-mustache:0.10")
 
