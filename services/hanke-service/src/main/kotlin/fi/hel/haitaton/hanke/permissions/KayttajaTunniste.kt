cc4225667821d31c2cf2f4855f1d04314c3e396b
Topias Heinonen <topias.heinonen@gofore.com>
2023-05-16 13:29:28 +0300

    Extra logging for user token creation (#327)
    More logging to User token generation

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/permissions/KayttajaTunniste.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/permissions/KayttajaTunniste.kt
@@ -12,8 +12,11 @@ import javax.persistence.Id
 import javax.persistence.OneToOne
 import javax.persistence.Table
 import kotlin.streams.asSequence
+import mu.KotlinLogging
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+
+private val logger = KotlinLogging.logger {}
 
 @Entity
 @Table(name = "kayttaja_tunniste")
@@ -39,12 +42,20 @@ class KayttajaTunnisteEntity(
         private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
         private val secureRandom: SecureRandom = SecureRandom.getInstanceStrong()
 
-        fun randomToken(): String =
-            secureRandom
-                .ints(tokenLength.toLong(), 0, charPool.size)
-                .asSequence()
-                .map(charPool::get)
-                .joinToString("")
+        fun randomToken(): String {
+            logger.info { "Generating random token" }
+            val token =
+                secureRandom
+                    .ints(tokenLength.toLong(), 0, charPool.size)
+                    .asSequence()
+                    .map(charPool::get)
+                    .joinToString("")
+            logger.info { "Random token generated" }
+            logger.info {
+                "java.security.egd = ${System.getProperty("java.security.egd")} and used the ${secureRandom.algorithm} algorithm"
+            }
+            return token
+        }
     }
 }
 
