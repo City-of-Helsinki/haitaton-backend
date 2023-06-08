e8691c1c52dac187d9147feecee7a1561b808db9
Topias Heinonen <topias.heinonen@gofore.com>
2023-06-08 10:26:47 +0300

    HAI-1748 Add more validation for application data
    Validations added:
    - There is at most one orderer
    - Text fields don't have just blanks
    - Country has a country code (UI uses constant FI here)
    
    On top of the earler:
    - Registry key (business-id) is valid
    - Start date is not after end date
    
    I ended up rewriting the validation using the same validators used in
    validating if a hanke is public or not. I refactored that quite a bit,
    though.

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/validation/Validators.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/validation/Validators.kt
@@ -1,8 +1,6 @@
 package fi.hel.haitaton.hanke.validation
 
-private typealias ValidationError = String
-
-private val ValidationSuccess: ValidationError? = null
+import java.time.ZonedDateTime
 
 /**
  * Validator helper methods for on-the-fly validation (not at the REST API-boundary or the database
@@ -11,67 +9,104 @@ private val ValidationSuccess: ValidationError? = null
  * Keeps track of the paths of validation failures, but not the errors that occurred.
  *
  * @see HankePublicValidator for examples on usage.
+ * @see ApplicationValidator for examples on usage.
  */
 object Validators {
 
     /**
      * Starts a validation chain. Further validations can be chained with [ValidationResult.and].
      */
-    fun validate(f: () -> ValidationError?): ValidationResult {
-        return ValidationResult().and(f)
+    fun validate(f: () -> ValidationResult): ValidationResult {
+        return ValidationResult.success().and(f)
     }
 
-    fun validateFalse(condition: Boolean, path: String): ValidationError? =
-        if (condition) path else ValidationSuccess
+    fun validateTrue(condition: Boolean, path: String): ValidationResult =
+        if (condition) ValidationResult.success() else ValidationResult.failure(path)
 
-    fun validateFalse(condition: Boolean?, path: String): ValidationError? =
-        if (condition == false) ValidationSuccess else path
+    fun validateFalse(condition: Boolean, path: String): ValidationResult =
+        if (!condition) ValidationResult.success() else ValidationResult.failure(path)
 
-    fun notNull(value: Any?, path: String): ValidationError? = validateFalse(value == null, path)
+    fun validateFalse(condition: Boolean?, path: String): ValidationResult =
+        if (condition == false) ValidationResult.success() else ValidationResult.failure(path)
 
-    fun notBlank(value: String, path: String): ValidationError? =
+    fun notNull(value: Any?, path: String): ValidationResult = validateFalse(value == null, path)
+
+    fun notBlank(value: String, path: String): ValidationResult =
         validateFalse(value.isBlank(), path)
 
-    fun notNullOrBlank(value: String?, path: String): ValidationError? =
+    /**
+     * Fails if the string is defined, non-empty and has nothing but whitespace characters.
+     * - null -> Ok
+     * - "" -> Ok
+     * - " " -> Fail
+     * - "*" -> Ok
+     * - "\t\n \t\n" -> Fail
+     * - " f " -> Ok
+     */
+    fun notJustWhitespace(value: String?, path: String): ValidationResult =
+        given(!value.isNullOrEmpty()) { notBlank(value!!, path) }
+
+    fun notNullOrBlank(value: String?, path: String): ValidationResult =
         validateFalse(value.isNullOrBlank(), path)
 
-    fun <T> notEmpty(value: Collection<T>, path: String): ValidationError? =
+    fun <T> notEmpty(value: Collection<T>, path: String): ValidationResult =
         validateFalse(value.isEmpty(), path)
 
-    fun <T> notNullOrEmpty(value: Collection<T>?, path: String): ValidationError? =
+    fun <T> notNullOrEmpty(value: Collection<T>?, path: String): ValidationResult =
         validateFalse(value?.isEmpty(), path)
 
-    /** Check run the validation lambda only if the pre-condition is true. */
-    fun given(condition: Boolean, f: () -> ValidationError?): ValidationError? =
-        if (condition) f() else ValidationSuccess
+    fun isBeforeOrEqual(start: ZonedDateTime, end: ZonedDateTime, path: String): ValidationResult =
+        validateFalse(start.isAfter(end), path)
+
+    /** Run the validation only if the pre-condition is true. */
+    fun given(condition: Boolean, f: () -> ValidationResult): ValidationResult =
+        if (condition) f() else ValidationResult.success()
 
     /**
      * Use only the first validation error from the ones supplied. Can be used to e.g. check for
      * null values somewhere along a path.
      */
-    fun firstOf(vararg fs: ValidationError?): ValidationError? =
-        fs.filterNotNull().firstOrNull() ?: ValidationSuccess
+    fun firstOf(vararg fs: ValidationResult): ValidationResult =
+        fs.toList().firstOrNull { !it.isOk() } ?: ValidationResult.success()
 }
 
-class ValidationResult {
-    private val errorPaths: MutableList<String> = mutableListOf()
+class ValidationResult
+private constructor(private val errorPaths: MutableList<String> = mutableListOf()) {
 
     fun errorPaths(): List<String> = errorPaths
     fun isOk() = errorPaths.isEmpty()
 
-    fun and(f: () -> ValidationError?): ValidationResult {
-        f()?.let { error -> this.errorPaths.add(error) }
+    fun and(f: () -> ValidationResult): ValidationResult {
+        this.errorPaths.addAll(f().errorPaths())
         return this
     }
+
+    /** Run the validation only when the value is not null. */
+    fun <T> whenNotNull(value: T?, f: (T) -> ValidationResult): ValidationResult {
+        if (value != null) {
+            and { f(value) }
+        }
+        return this
+    }
+
+    /** Check run the validation lambda only if the pre-condition is true. */
+    fun andWhen(condition: Boolean, f: () -> ValidationResult): ValidationResult =
+        if (condition) this.and(f) else this
 
     fun <T> andAllIn(
         values: Collection<T>,
         path: String,
-        f: (T, String) -> ValidationResult
+        f: (T, String) -> ValidationResult,
     ): ValidationResult {
         this.errorPaths.addAll(
             values.flatMapIndexed { index: Int, value: T -> f(value, "$path[$index]").errorPaths() }
         )
         return this
+    }
+
+    companion object {
+        fun success() = ValidationResult()
+
+        fun failure(errorPath: String) = ValidationResult(mutableListOf(errorPath))
     }
 }
