f74285337320327d238568604113a7cddaf6a870
Topias Heinonen <topias.heinonen@gofore.com>
2023-08-07 10:54:49 +0300

    HAI-1638 Remove GeometriaController (#379)
    The concept of hanke-geometria has been superseded by the concept of
    hankealue. One of the old geometria-endpoints was still up, but unused.
    
    Remove the remaining geometria-endpoint, and the since it's the last
    method in the controller, the whole controller. Also, remove the
    loadGeometriat method, which wasn't used anywhere else.
    
    Refactor `HankeControllerITests`:
    - Divide the tests for different endpoints into different inner classes.
    - Move HankeControllerHankeEditingDisabledITests to a separate file.
    - Move the unauthorized tests from HankeControllerSecurityTests to
      HankeControllerITests.
    - Remove the positive tests from HankeControllerSecurityTests, since
      they were duplicates for tests in HankeControllerITests.
      - Delete HankeControllerSecurityTests, since it was now empty.
    - Use verifySequence instead of many separate verify-blocks.
    - Introduce a ResultMatcher for checking that a response is a specific
      HankeError, based on a private method in HankeControllerSecurityTests.
      - Use it in all tests in HankeControllerSecurityTests
      - This should be used more in tests. Just checking for a status might
        lead to false positives, since we don't know what caused the status.
    - Fix the test for checking that a perustaja without email is not
      allowed.

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/geometria/GeometriatService.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/geometria/GeometriatService.kt
@@ -1,13 +1,8 @@
 package fi.hel.haitaton.hanke.geometria
 
-import fi.hel.haitaton.hanke.domain.Hanke
-
 interface GeometriatService {
     /** Insert/Update geometries. */
     fun saveGeometriat(geometriat: Geometriat): Geometriat?
-
-    /** Loads all Geometriat under Hanke. */
-    fun loadGeometriat(hanke: Hanke): Geometriat?
 
     /** Loads single geometry object. */
     fun getGeometriat(geometriatId: Int): Geometriat?
