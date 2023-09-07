feb648298a5c94bd874c094e7daa152f0ac91cce
Topias Heinonen <topias.heinonen@gofore.com>
2023-09-07 13:39:30 +0300

    HAI-1516 Remove version plugin, dependabot fills that role

--- services/hanke-service/build.gradle.kts
+++ services/hanke-service/build.gradle.kts
@@ -57,7 +57,6 @@ plugins {
     // Gives kotlin-noarg for @Entity, @Embeddable
     kotlin("plugin.jpa") version "1.8.22"
     idea
-    id("com.github.ben-manes.versions") version "0.42.0"
     id("jacoco")
 }
 
