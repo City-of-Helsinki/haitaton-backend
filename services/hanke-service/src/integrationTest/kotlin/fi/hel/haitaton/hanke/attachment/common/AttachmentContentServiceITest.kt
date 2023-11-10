ccf00a5c0d7a45513abb9d4f71f2be7d9f4e7eb5
Topias Heinonen <topias.heinonen@gofore.com>
2023-11-10 14:27:05 +0200

    fixup! HAI-1969 Download hanke attachments from Blob Storage

--- services/hanke-service/src/integrationTest/kotlin/fi/hel/haitaton/hanke/attachment/common/AttachmentContentServiceITest.kt
+++ services/hanke-service/src/integrationTest/kotlin/fi/hel/haitaton/hanke/attachment/common/AttachmentContentServiceITest.kt
@@ -18,9 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
-import org.testcontainers.junit.jupiter.Testcontainers
 
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @WithMockUser(USERNAME)
