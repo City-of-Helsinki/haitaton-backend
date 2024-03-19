95438a12f4f473b8414d7b215c9d812c5b09d833
Topias Heinonen <topias.heinonen@gofore.com>
2024-03-19 13:11:16 +0200

    fixup! HAI-2301 Remove HakemusyhteystietoBuilder

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/hakemus/HakemusService.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/hakemus/HakemusService.kt
@@ -38,14 +38,6 @@ class HakemusService(
     private val hakemusyhteyshenkiloRepository: HakemusyhteyshenkiloRepository,
 ) {
     @Transactional(readOnly = true)
-    fun get(applicationId: Long): Hakemus {
-        val applicationEntity =
-            applicationRepository.findOneById(applicationId)
-                ?: throw ApplicationNotFoundException(applicationId)
-        return applicationEntity.toHakemus()
-    }
-
-    @Transactional(readOnly = true)
     fun hakemusResponse(applicationId: Long): HakemusResponse {
         val applicationEntity =
             applicationRepository.findOneById(applicationId)
