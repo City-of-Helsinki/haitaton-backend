0eb73f2f56ed1b44c27ad3300d2849c45c794072
Topias Heinonen <topias.heinonen@gofore.com>
2024-05-06 13:39:48 +0300

    HAI-2345 Only allow PDFs for attachments other than MUU
    Only accept PDF attachments for `LIIKENNEJARJESTELY` and `VALTAKIRJA`
    type attachments. Traffic arrangements need to be PDFs since they are
    attached to the decision in Allu. The valtakirja needs to be a PDF since
    it needs to be signed.
    
    Change the consumes parameters for the attachment controllers. This
    enables attachment uploading in SwaggerUI.

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/attachment/application/ApplicationAttachmentController.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/attachment/application/ApplicationAttachmentController.kt
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import java.util.UUID
 import mu.KotlinLogging
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -85,7 +86,7 @@ class ApplicationAttachmentController(
             .body(content.bytes)
     }
 
-    @PostMapping
+    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     @Operation(
         summary = "Upload attachment for application",
         description =
