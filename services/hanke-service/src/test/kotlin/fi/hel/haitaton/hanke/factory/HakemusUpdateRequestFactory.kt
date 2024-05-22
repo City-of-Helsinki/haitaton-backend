146894c0edabf56abf93b857a359128cf92951a4
Topias Heinonen <topias.heinonen@gofore.com>
2024-05-22 09:52:21 +0300

    HAI-2310 Move and rename ApplicationArea (#737)
    Rename ApplicationArea to Hakemusalue and move it under the hakemus
    package.

--- services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/factory/HakemusUpdateRequestFactory.kt
+++ services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/factory/HakemusUpdateRequestFactory.kt
@@ -2,9 +2,6 @@ package fi.hel.haitaton.hanke.factory
 
 import fi.hel.haitaton.hanke.TZ_UTC
 import fi.hel.haitaton.hanke.allu.CustomerType
-import fi.hel.haitaton.hanke.application.ApplicationArea
-import fi.hel.haitaton.hanke.application.CableReportApplicationArea
-import fi.hel.haitaton.hanke.application.ExcavationNotificationArea
 import fi.hel.haitaton.hanke.application.StreetAddress
 import fi.hel.haitaton.hanke.factory.ApplicationFactory.Companion.createCableReportApplicationArea
 import fi.hel.haitaton.hanke.factory.ApplicationFactory.Companion.createExcavationNotificationArea
@@ -15,9 +12,12 @@ import fi.hel.haitaton.hanke.hakemus.CustomerRequest
 import fi.hel.haitaton.hanke.hakemus.CustomerWithContactsRequest
 import fi.hel.haitaton.hanke.hakemus.HakemusResponse
 import fi.hel.haitaton.hanke.hakemus.HakemusUpdateRequest
+import fi.hel.haitaton.hanke.hakemus.Hakemusalue
 import fi.hel.haitaton.hanke.hakemus.InvoicingCustomerRequest
 import fi.hel.haitaton.hanke.hakemus.InvoicingPostalAddressRequest
+import fi.hel.haitaton.hanke.hakemus.JohtoselvitysHakemusalue
 import fi.hel.haitaton.hanke.hakemus.JohtoselvityshakemusUpdateRequest
+import fi.hel.haitaton.hanke.hakemus.KaivuilmoitusAlue
 import fi.hel.haitaton.hanke.hakemus.KaivuilmoitusUpdateRequest
 import fi.hel.haitaton.hanke.hakemus.PostalAddressRequest
 import fi.hel.haitaton.hanke.parseJson
@@ -272,14 +272,14 @@ object HakemusUpdateRequestFactory {
             is KaivuilmoitusUpdateRequest -> this.copy(requiredCompetence = requiredCompetence)
         }
 
-    fun HakemusUpdateRequest.withArea(area: ApplicationArea?) = withAreas(area?.let { listOf(it) })
+    fun HakemusUpdateRequest.withArea(area: Hakemusalue?) = withAreas(area?.let { listOf(it) })
 
-    fun HakemusUpdateRequest.withAreas(areas: List<ApplicationArea>?) =
+    fun HakemusUpdateRequest.withAreas(areas: List<Hakemusalue>?) =
         when (this) {
             is JohtoselvityshakemusUpdateRequest ->
-                this.copy(areas = areas?.map { it as CableReportApplicationArea })
+                this.copy(areas = areas?.map { it as JohtoselvitysHakemusalue })
             is KaivuilmoitusUpdateRequest ->
-                this.copy(areas = areas?.map { it as ExcavationNotificationArea })
+                this.copy(areas = areas?.map { it as KaivuilmoitusAlue })
         }
 
     fun HakemusUpdateRequest.withTimes(startTime: ZonedDateTime?, endTime: ZonedDateTime?) =
