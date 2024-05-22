3a0038e27072fe1dfb819ad124135ff7b9583b06
Topias Heinonen <topias.heinonen@gofore.com>
2024-05-22 09:19:35 +0300

    HAI-2310 Move and rename ApplicationAuthorizer (#729)
    Move `ApplicationAuthorizer` to `hakemus` package and rename it to
    `HakemusAuthorizer`. Rename the functions accordingly.

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/hakemus/HakemusAuthorizer.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/hakemus/HakemusAuthorizer.kt
@@ -1,9 +1,10 @@
-package fi.hel.haitaton.hanke.application
+package fi.hel.haitaton.hanke.hakemus
 
 import fi.hel.haitaton.hanke.HankeRepository
+import fi.hel.haitaton.hanke.application.ApplicationNotFoundException
+import fi.hel.haitaton.hanke.application.ApplicationRepository
 import fi.hel.haitaton.hanke.attachment.common.ApplicationAttachmentRepository
 import fi.hel.haitaton.hanke.attachment.common.AttachmentNotFoundException
-import fi.hel.haitaton.hanke.hakemus.CreateHakemusRequest
 import fi.hel.haitaton.hanke.permissions.Authorizer
 import fi.hel.haitaton.hanke.permissions.PermissionCode
 import fi.hel.haitaton.hanke.permissions.PermissionService
@@ -15,40 +16,33 @@ import org.springframework.transaction.annotation.Transactional
 
 @Component
 @Primary
-class ApplicationAuthorizer(
+class HakemusAuthorizer(
     permissionService: PermissionService,
     hankeRepository: HankeRepository,
     private val applicationRepository: ApplicationRepository,
     private val attachmentRepository: ApplicationAttachmentRepository,
 ) : Authorizer(permissionService, hankeRepository) {
-    private fun authorizeApplicationId(
-        applicationId: Long,
-        permissionCode: PermissionCode
-    ): Boolean {
-        val hankeId = applicationRepository.findOneById(applicationId)?.hanke?.id
-        authorize(hankeId, permissionCode) { ApplicationNotFoundException(applicationId) }
+    private fun authorizeHakemusId(hakemusId: Long, permissionCode: PermissionCode): Boolean {
+        val hankeId = applicationRepository.findOneById(hakemusId)?.hanke?.id
+        authorize(hankeId, permissionCode) { ApplicationNotFoundException(hakemusId) }
         return true
     }
 
     @Transactional(readOnly = true)
-    fun authorizeApplicationId(applicationId: Long, permissionCode: String): Boolean =
-        authorizeApplicationId(applicationId, PermissionCode.valueOf(permissionCode))
+    fun authorizeHakemusId(hakemusId: Long, permissionCode: String): Boolean =
+        authorizeHakemusId(hakemusId, PermissionCode.valueOf(permissionCode))
 
     @Transactional(readOnly = true)
     fun authorizeCreate(hakemus: CreateHakemusRequest): Boolean =
         authorizeHankeTunnus(hakemus.hankeTunnus, PermissionCode.EDIT_APPLICATIONS)
 
     @Transactional(readOnly = true)
-    fun authorizeAttachment(
-        applicationId: Long,
-        attachmentId: UUID,
-        permissionCode: String
-    ): Boolean {
-        authorizeApplicationId(applicationId, permissionCode)
-        return findApplicationIdForAttachment(attachmentId) == applicationId ||
+    fun authorizeAttachment(hakemusId: Long, attachmentId: UUID, permissionCode: String): Boolean {
+        authorizeHakemusId(hakemusId, permissionCode)
+        return findHakemusIdForAttachment(attachmentId) == hakemusId ||
             throw AttachmentNotFoundException(attachmentId)
     }
 
-    private fun findApplicationIdForAttachment(attachmentId: UUID): Long? =
+    private fun findHakemusIdForAttachment(attachmentId: UUID): Long? =
         attachmentRepository.findByIdOrNull(attachmentId)?.applicationId
 }
