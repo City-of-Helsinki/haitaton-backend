b949e2612cbde2d4f8752c2d68da566327fcb028
Topias Heinonen <topias.heinonen@gofore.com>
2024-10-18 12:34:47 +0300

    HAI-2749 Use data instead of entity for hakemus update change check
    Compare the hakemus update to a ` HakemusData` instead of an
    `ApplicationEntity` when checking if the update has any changes.
    
    This allows the same method to be used to check if a täydennys has any
    changes over the original hakemus or the previously saved täydennys.

--- services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/hakemus/HakemusUpdateRequestTest.kt
+++ services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/hakemus/HakemusUpdateRequestTest.kt
@@ -5,15 +5,12 @@ import assertk.assertions.isFalse
 import assertk.assertions.isTrue
 import fi.hel.haitaton.hanke.allu.CustomerType
 import fi.hel.haitaton.hanke.factory.ApplicationFactory
-import fi.hel.haitaton.hanke.factory.ApplicationFactory.Companion.createApplicationEntity
 import fi.hel.haitaton.hanke.factory.ApplicationFactory.Companion.createBlankExcavationNotificationData
 import fi.hel.haitaton.hanke.factory.ApplicationFactory.Companion.createExcavationNotificationArea
 import fi.hel.haitaton.hanke.factory.ApplicationFactory.Companion.createTyoalue
 import fi.hel.haitaton.hanke.factory.HakemusUpdateRequestFactory
 import fi.hel.haitaton.hanke.factory.HakemusyhteyshenkiloFactory
 import fi.hel.haitaton.hanke.factory.HakemusyhteystietoFactory
-import fi.hel.haitaton.hanke.factory.HankeFactory
-import fi.hel.haitaton.hanke.factory.HankeKayttajaFactory
 import fi.hel.haitaton.hanke.factory.HankealueFactory.TORMAYSTARKASTELU_DEFAULT_AUTOLIIKENNELUOKITTELU
 import fi.hel.haitaton.hanke.tormaystarkastelu.TormaystarkasteluTulos
 import java.util.UUID
@@ -21,60 +18,46 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class HakemusUpdateRequestTest {
+    private val blankOriginal =
+        ApplicationFactory.createBlankCableReportApplicationData()
+            .toHakemusData(yhteystiedot = mapOf())
+
+    private val blankRequest =
+        HakemusUpdateRequestFactory.createBlankJohtoselvityshakemusUpdateRequest()
 
     @Nested
     inner class HasChanges {
 
         @Test
         fun `returns false when nothing has changed`() {
-            val original =
-                ApplicationFactory.createApplicationEntity(
-                    hakemusEntityData = ApplicationFactory.createBlankCableReportApplicationData(),
-                    hanke = HankeFactory.createMinimalEntity(),
-                )
-            val request = HakemusUpdateRequestFactory.createBlankJohtoselvityshakemusUpdateRequest()
+            val original = blankOriginal
+            val request = blankRequest
 
             assertThat(request.hasChanges(original)).isFalse()
         }
 
         @Test
         fun `returns true when name is changed`() {
-            val original =
-                ApplicationFactory.createApplicationEntity(
-                    hakemusEntityData = ApplicationFactory.createBlankCableReportApplicationData(),
-                    hanke = HankeFactory.createMinimalEntity(),
-                )
-            val request =
-                HakemusUpdateRequestFactory.createBlankJohtoselvityshakemusUpdateRequest()
-                    .copy(name = "Testihakemus")
+            val original = blankOriginal
+            val request = blankRequest.copy(name = "Testihakemus")
 
             assertThat(request.hasChanges(original)).isTrue()
         }
 
         @Test
         fun `returns true when work description is changed`() {
-            val original =
-                ApplicationFactory.createApplicationEntity(
-                    hakemusEntityData = ApplicationFactory.createBlankCableReportApplicationData(),
-                    hanke = HankeFactory.createMinimalEntity(),
-                )
+            val original = blankOriginal
             val request =
-                HakemusUpdateRequestFactory.createBlankJohtoselvityshakemusUpdateRequest()
-                    .copy(
-                        workDescription =
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                    )
+                blankRequest.copy(
+                    workDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                )
 
             assertThat(request.hasChanges(original)).isTrue()
         }
 
         @Test
         fun `returns true when a customer is added`() {
-            val original =
-                ApplicationFactory.createApplicationEntity(
-                    hakemusEntityData = ApplicationFactory.createBlankCableReportApplicationData(),
-                    hanke = HankeFactory.createMinimalEntity(),
-                )
+            val original = blankOriginal
             val request =
                 createJohtoselvityshakemusUpdateRequestWithCustomerWithContacts(
                     "cd1d4d2f-526b-4ee5-a1fa-97b14d25a11f",
@@ -86,7 +69,7 @@ class HakemusUpdateRequestTest {
         @Test
         fun `returns true when new customer contact is added`() {
             val original =
-                createApplicationEntityWithYhteystiedot(
+                createHakemusDataWithYhteystiedot(
                     Pair("cd1d4d2f-526b-4ee5-a1fa-97b14d25a11f", true),
                 )
             val request =
@@ -101,7 +84,7 @@ class HakemusUpdateRequestTest {
         @Test
         fun `returns false when customer contacts are the same`() {
             val original =
-                createApplicationEntityWithYhteystiedot(
+                createHakemusDataWithYhteystiedot(
                     Pair("cd1d4d2f-526b-4ee5-a1fa-97b14d25a11f", true),
                     Pair("3047a6fc-5a2b-41cb-bb99-1f907fef2101", false),
                 )
@@ -117,7 +100,7 @@ class HakemusUpdateRequestTest {
         @Test
         fun `returns true when customer contacts are removed`() {
             val original =
-                createApplicationEntityWithYhteystiedot(
+                createHakemusDataWithYhteystiedot(
                     Pair("cd1d4d2f-526b-4ee5-a1fa-97b14d25a11f", true),
                     Pair("3047a6fc-5a2b-41cb-bb99-1f907fef2101", false),
                 )
@@ -128,64 +111,52 @@ class HakemusUpdateRequestTest {
 
         @Test
         fun `returns false when only tormaystarkastelu is missing in request`() {
-            val original = createApplicationEntityWithTormaystarkastelu()
+            val original = createHakemusDataWithTormaystarkastelu()
             val request = createKaivuilmoitusUpdateRequestWithoutTormaystarkastelu()
             assertThat(request.hasChanges(original)).isFalse()
         }
     }
 
-    private fun createApplicationEntityWithYhteystiedot(
-        vararg hakemusyhteyshenkilot: Pair<String, Boolean>
-    ): HakemusEntity =
-        ApplicationFactory.createApplicationEntity(
-                hakemusEntityData = ApplicationFactory.createBlankCableReportApplicationData(),
-                hanke = HankeFactory.createMinimalEntity(),
-            )
-            .apply {
-                yhteystiedot[ApplicationContactType.HAKIJA] =
-                    HakemusyhteystietoFactory.createEntity(
-                            tyyppi = CustomerType.COMPANY,
-                            rooli = ApplicationContactType.HAKIJA,
-                            nimi = "Testiyritys",
-                            sahkoposti = "info@testiyritys.fi",
-                            puhelinnumero = "0401234567",
-                            ytunnus = "1234567-8",
-                            application = this,
-                        )
-                        .apply {
-                            yhteyshenkilot.addAll(
-                                hakemusyhteyshenkilot.map {
-                                    HakemusyhteyshenkiloFactory.createEntity(
-                                        this,
-                                        HankeKayttajaFactory.createEntity(
-                                            id = UUID.fromString(it.first),
-                                        ),
-                                        it.second,
-                                    )
-                                },
-                            )
-                        }
+    private fun createHakemusDataWithYhteystiedot(
+        vararg hankekayttajat: Pair<String, Boolean>
+    ): HakemusData {
+        val yhteyshenkilot =
+            hankekayttajat.map { (hankekayttajaId, tilaaja) ->
+                HakemusyhteyshenkiloFactory.create(
+                    hankekayttajaId = UUID.fromString(hankekayttajaId), tilaaja = tilaaja)
             }
 
-    private fun createJohtoselvityshakemusUpdateRequestWithCustomerWithContacts(
-        vararg yhteyshenkilot: String
-    ): JohtoselvityshakemusUpdateRequest =
-        HakemusUpdateRequestFactory.createBlankJohtoselvityshakemusUpdateRequest()
-            .copy(
-                customerWithContacts =
-                    CustomerWithContactsRequest(
-                        CustomerRequest(
-                            type = CustomerType.COMPANY,
-                            name = "Testiyritys",
-                            email = "info@testiyritys.fi",
-                            phone = "0401234567",
-                            registryKey = "1234567-8",
-                        ),
-                        yhteyshenkilot.map { ContactRequest(UUID.fromString(it)) },
-                    ),
+        val yhteystieto =
+            HakemusyhteystietoFactory.create(
+                tyyppi = CustomerType.COMPANY,
+                rooli = ApplicationContactType.HAKIJA,
+                nimi = "Testiyritys",
+                sahkoposti = "info@testiyritys.fi",
+                puhelinnumero = "0401234567",
+                registryKey = "1234567-8",
+                yhteyshenkilot = yhteyshenkilot,
             )
 
-    private fun createApplicationEntityWithTormaystarkastelu(): HakemusEntity {
+        return blankOriginal.copy(customerWithContacts = yhteystieto)
+    }
+
+    private fun createJohtoselvityshakemusUpdateRequestWithCustomerWithContacts(
+        vararg hankekayttajaIds: String
+    ): JohtoselvityshakemusUpdateRequest {
+        val customer =
+            CustomerRequest(
+                type = CustomerType.COMPANY,
+                name = "Testiyritys",
+                email = "info@testiyritys.fi",
+                phone = "0401234567",
+                registryKey = "1234567-8",
+            )
+        val contacts = hankekayttajaIds.map { ContactRequest(UUID.fromString(it)) }
+        val customerWithContacts = CustomerWithContactsRequest(customer, contacts)
+        return blankRequest.copy(customerWithContacts = customerWithContacts)
+    }
+
+    private fun createHakemusDataWithTormaystarkastelu(): HakemusData {
         val tormaystarkasteluTulos =
             TormaystarkasteluTulos(
                 TORMAYSTARKASTELU_DEFAULT_AUTOLIIKENNELUOKITTELU,
@@ -195,12 +166,9 @@ class HakemusUpdateRequestTest {
             )
         val tyoalue = createTyoalue(tormaystarkasteluTulos = tormaystarkasteluTulos)
         val area = createExcavationNotificationArea(tyoalueet = listOf(tyoalue))
-        val hakemusEntityData = createBlankExcavationNotificationData().copy(areas = listOf(area))
-        return createApplicationEntity(
-            applicationType = ApplicationType.EXCAVATION_NOTIFICATION,
-            hakemusEntityData = hakemusEntityData,
-            hanke = HankeFactory.createMinimalEntity(),
-        )
+        return createBlankExcavationNotificationData()
+            .copy(areas = listOf(area))
+            .toHakemusData(mapOf())
     }
 
     private fun createKaivuilmoitusUpdateRequestWithoutTormaystarkastelu():
