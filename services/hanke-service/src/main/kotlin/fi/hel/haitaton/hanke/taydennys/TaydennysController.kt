3838cefd1894758841c7e478a0b15aad7d108dff
Topias Heinonen <topias.heinonen@gofore.com>
2024-10-21 10:02:30 +0300

    HAI-2749 Add API for updating a täydennys
    Add API for updating a täydennys. The API has most of the same checks
    and functionalities as the hakemus update. I tried to reuse as much of
    the code in HakemusService as I could. However, some methods that alter
    the database entities have to access a specific entity, so they can't be
    shared easily.
    
    One check removed from the täydennys update is checking if the hakemus
    has been sent to Allu. It always has. We don't check the hakemus status
    at all, since the täydennys shouldn't exist unless the hakemus is in a
    compatible state. Even if it's not due to a bug, there's no harm in
    updating the täydennys. When sending the täydennys and merging the data
    back to the hakemus we need to be more careful with the status.
    
    Updating the name of a generated hanke will be done when the täydennys
    is sent and merged to the hakemus.

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/taydennys/TaydennysController.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/taydennys/TaydennysController.kt
@@ -1,14 +1,20 @@
 package fi.hel.haitaton.hanke.taydennys
 
 import fi.hel.haitaton.hanke.HankeError
+import fi.hel.haitaton.hanke.HankeErrorDetail
 import fi.hel.haitaton.hanke.currentUserId
+import fi.hel.haitaton.hanke.hakemus.HakemusUpdateRequest
+import fi.hel.haitaton.hanke.hakemus.InvalidHakemusDataException
+import fi.hel.haitaton.hanke.hakemus.ValidHakemusUpdateRequest
 import io.swagger.v3.oas.annotations.Hidden
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import java.util.UUID
 import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
@@ -16,6 +22,8 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
@@ -54,11 +62,85 @@ class TaydennysController(private val taydennysService: TaydennysService) {
     fun create(@PathVariable id: Long): TaydennysResponse =
         taydennysService.create(id, currentUserId()).toResponse()
 
+    @RequestMapping("/taydennykset/{id}")
+    @Operation(
+        summary = "Update a täydennys",
+        description =
+            """
+               Returns the updated täydennys.
+               The täydennys can be updated until it has been sent to Allu.
+               If the täydennys hasn't changed since the last update, nothing more is done.
+            """,
+    )
+    @ApiResponses(
+        value =
+            [
+                ApiResponse(description = "The updated täydennys", responseCode = "200"),
+                ApiResponse(
+                    description = "Request contains invalid data",
+                    responseCode = "400",
+                    content =
+                        [
+                            Content(
+                                schema =
+                                    Schema(oneOf = [HankeErrorDetail::class, HankeError::class]),
+                                examples =
+                                    [
+                                        ExampleObject(
+                                            name = "Validation error",
+                                            summary = "Validation error example",
+                                            value = "{hankeError: 'HAI2008', errorPaths: ['name']}",
+                                        ),
+                                        ExampleObject(
+                                            name = "Incompatible request",
+                                            summary = "Incompatible request example",
+                                            value = "{hankeError: 'HAI2002'}",
+                                        ),
+                                    ],
+                            )],
+                ),
+                ApiResponse(
+                    description = "A täydennys was not found with the given id",
+                    responseCode = "404",
+                    content = [Content(schema = Schema(implementation = HankeError::class))],
+                ),
+            ])
+    @PreAuthorize("@taydennysAuthorizer.authorize(#id, 'EDIT_APPLICATIONS')")
+    fun update(
+        @PathVariable id: UUID,
+        @ValidHakemusUpdateRequest @RequestBody request: HakemusUpdateRequest,
+    ): TaydennysResponse =
+        taydennysService.updateTaydennys(id, request, currentUserId()).toResponse()
+
+    @ExceptionHandler(InvalidHakemusDataException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @Hidden
+    fun invalidHakemusDataException(ex: InvalidHakemusDataException): HankeErrorDetail {
+        logger.warn(ex) { ex.message }
+        return HankeErrorDetail(hankeError = HankeError.HAI2008, errorPaths = ex.errorPaths)
+    }
+
     @ExceptionHandler(NoTaydennyspyyntoException::class)
     @ResponseStatus(HttpStatus.CONFLICT)
     @Hidden
     fun noTaydennyspyyntoException(ex: NoTaydennyspyyntoException): HankeError {
         logger.warn(ex) { ex.message }
         return HankeError.HAI2015
+    }
+
+    @ExceptionHandler(TaydennysNotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @Hidden
+    fun taydennysNotFoundException(ex: TaydennysNotFoundException): HankeError {
+        logger.warn(ex) { ex.message }
+        return HankeError.HAI6001
+    }
+
+    @ExceptionHandler(IncompatibleTaydennysUpdateException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @Hidden
+    fun incompatibleTaydennysUpdateException(ex: IncompatibleTaydennysUpdateException): HankeError {
+        logger.warn(ex) { ex.message }
+        return HankeError.HAI2002
     }
 }
