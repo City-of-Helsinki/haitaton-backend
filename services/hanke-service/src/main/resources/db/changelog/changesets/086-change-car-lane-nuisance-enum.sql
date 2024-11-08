1184ad915beccfbc1e35cadb6403f415fd686b03
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2024-11-08 15:11:09 +0200

    fixup! fixup! fixup! fixup! HAI-2815 Adjust enum values for kaistahaitta and kaistapituushaitta. Migrate old values into new ones in database. Does not recalculate tormaystarkastelu in database with new values.

--- services/hanke-service/src/main/resources/db/changelog/changesets/086-change-car-lane-nuisance-enum.sql
+++ services/hanke-service/src/main/resources/db/changelog/changesets/086-change-car-lane-nuisance-enum.sql
@@ -13,22 +13,6 @@ UPDATE hankealue
             ELSE kaistahaitta
         END;
 
--- update tormaystarkastelutulos.kaistapituushaitta
-UPDATE tormaystarkastelutulos
-    SET kaistapituushaitta=0
-    WHERE kaistapituushaitta=1;
-
--- update tormaystarkastelutulos.kaistahaitta
-UPDATE tormaystarkastelutulos
-    SET kaistahaitta =
-        CASE kaistahaitta
-            WHEN 1 THEN 0
-            WHEN 2 THEN 1
-            WHEN 3 THEN 2
-            WHEN 4 THEN 3
-            ELSE kaistahaitta
-        END;
-
 -- update applications.applicationData.areas
 WITH areas_array AS (
     SELECT id,
@@ -39,79 +23,23 @@ WITH areas_array AS (
                     '{areas}',
                     (
                         SELECT jsonb_agg(
-                            CASE
-                                WHEN (area #>> '{tyoalueet}') IS NOT NULL AND jsonb_typeof(area->'tyoalueet') = 'array' THEN
-                                    -- Update area level kaistahaitta first
-                                    jsonb_set(
-                                        jsonb_set(
-                                            area,
-                                            '{tyoalueet}',
-                                            (
-                                                SELECT jsonb_agg(
-                                                    CASE
-                                                        WHEN (tyoalue #>> '{tormaystarkasteluTulos}') IS NOT NULL AND
-                                                            (tyoalue #>> '{tormaystarkasteluTulos,autoliikenne}') IS NOT NULL THEN
-                                                            CASE
-                                                                -- First update kaistapituushaitta if it's 1
-                                                                WHEN tyoalue->'tormaystarkasteluTulos'->'autoliikenne'->>'kaistapituushaitta' = '1' THEN
-                                                                    jsonb_set(
-                                                                        jsonb_set(
-                                                                            tyoalue,
-                                                                            '{tormaystarkasteluTulos,autoliikenne,kaistapituushaitta}',
-                                                                            '0'::jsonb
-                                                                        ),
-                                                                        '{tormaystarkasteluTulos,autoliikenne,kaistahaitta}',
-                                                                        (
-                                                                            CASE tyoalue->'tormaystarkasteluTulos'->'autoliikenne'->>'kaistahaitta'
-                                                                                WHEN '1' THEN '0'::jsonb
-                                                                                WHEN '2' THEN '1'::jsonb
-                                                                                WHEN '3' THEN '2'::jsonb
-                                                                                WHEN '4' THEN '3'::jsonb
-                                                                                WHEN '5' THEN '5'::jsonb
-                                                                                ELSE tyoalue->'tormaystarkasteluTulos'->'autoliikenne'->'kaistahaitta'
-                                                                            END
-                                                                        )
-                                                                    )
-                                                                ELSE
-                                                                    -- Only update kaistahaitta
-                                                                    jsonb_set(
-                                                                        tyoalue,
-                                                                        '{tormaystarkasteluTulos,autoliikenne,kaistahaitta}',
-                                                                        (
-                                                                            CASE tyoalue->'tormaystarkasteluTulos'->'autoliikenne'->>'kaistahaitta'
-                                                                                WHEN '1' THEN '0'::jsonb
-                                                                                WHEN '2' THEN '1'::jsonb
-                                                                                WHEN '3' THEN '2'::jsonb
-                                                                                WHEN '4' THEN '3'::jsonb
-                                                                                WHEN '5' THEN '5'::jsonb
-                                                                                ELSE tyoalue->'tormaystarkasteluTulos'->'autoliikenne'->'kaistahaitta'
-                                                                            END
-                                                                        )
-                                                                    )
-                                                            END
-                                                        ELSE tyoalue
-                                                    END
-                                                )
-                                                FROM jsonb_array_elements(area->'tyoalueet') tyoalue
-                                            )
-                                        ),
-                                        '{kaistahaitta}',
-                                        (
-                                            CASE area->>'kaistahaitta'
-                                                WHEN 'VAHENTAA_KAISTAN_YHDELLA_AJOSUUNNALLA' THEN
-                                                    '"YKSI_KAISTA_VAHENEE"'::jsonb
-                                                WHEN 'VAHENTAA_SAMANAIKAISESTI_KAISTAN_KAHDELLA_AJOSUUNNALLA' THEN
-                                                    '"YKSI_KAISTA_VAHENEE_KAHDELLA_AJOSUUNNALLA"'::jsonb
-                                                WHEN 'VAHENTAA_SAMANAIKAISESTI_USEITA_KAISTOJA_KAHDELLA_AJOSUUNNALLA' THEN
-                                                    '"USEITA_KAISTOJA_VAHENEE_AJOSUUNNILLA"'::jsonb
-                                                WHEN 'VAHENTAA_SAMANAIKAISESTI_USEITA_KAISTOJA_LIITTYMIEN_ERI_SUUNNILLA' THEN
-                                                    '"USEITA_AJOSUUNTIA_POISTUU_KAYTOSTA"'::jsonb
-                                                ELSE area->'kaistahaitta'
-                                            END
-                                        )
-                                    )
-                                ELSE area
-                            END
+                            jsonb_set(
+                                area,
+                                '{kaistahaitta}',
+                                (
+                                    CASE area->>'kaistahaitta'
+                                        WHEN 'VAHENTAA_KAISTAN_YHDELLA_AJOSUUNNALLA' THEN
+                                            '"YKSI_KAISTA_VAHENEE"'::jsonb
+                                        WHEN 'VAHENTAA_SAMANAIKAISESTI_KAISTAN_KAHDELLA_AJOSUUNNALLA' THEN
+                                            '"YKSI_KAISTA_VAHENEE_KAHDELLA_AJOSUUNNALLA"'::jsonb
+                                        WHEN 'VAHENTAA_SAMANAIKAISESTI_USEITA_KAISTOJA_KAHDELLA_AJOSUUNNALLA' THEN
+                                            '"USEITA_KAISTOJA_VAHENEE_AJOSUUNNILLA"'::jsonb
+                                        WHEN 'VAHENTAA_SAMANAIKAISESTI_USEITA_KAISTOJA_LIITTYMIEN_ERI_SUUNNILLA' THEN
+                                            '"USEITA_AJOSUUNTIA_POISTUU_KAYTOSTA"'::jsonb
+                                        ELSE area->'kaistahaitta'
+                                    END
+                                )
+                            )
                         )
                         FROM jsonb_array_elements(applicationData->'areas') area
                     )
