f9c5b11c3045834f9c7b72849a7bfdc17cc8d745
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2024-11-22 10:39:27 +0200

    fixup! HAI-2871 Correctly updates applications.applicationData.areas without accidently deleting applicationData or applicationData.areas or other applicationData fields

--- services/hanke-service/src/main/resources/db/changelog/changesets/086-change-car-lane-nuisance-enum.sql
+++ services/hanke-service/src/main/resources/db/changelog/changesets/086-change-car-lane-nuisance-enum.sql
@@ -14,40 +14,31 @@ UPDATE hankealue
         END;
 
 -- update applications.applicationData.areas
-WITH updated_areas AS (
-    SELECT id,
-        CASE
-            WHEN (applicationData #>> '{areas}') IS NOT NULL AND jsonb_typeof(applicationData->'areas') = 'array' THEN
-                jsonb_set(
-                    applicationData,
-                    '{areas}',
-                    jsonb_agg(
-                        CASE
-                            WHEN (area #>> '{kaistahaitta}') IS NOT NULL THEN
-                                jsonb_set(
-                                    area,
-                                    '{kaistahaitta}',
-                                    CASE area->>'kaistahaitta'
-                                        WHEN 'VAHENTAA_KAISTAN_YHDELLA_AJOSUUNNALLA' THEN '"YKSI_KAISTA_VAHENEE"'::jsonb
-                                        WHEN 'VAHENTAA_SAMANAIKAISESTI_KAISTAN_KAHDELLA_AJOSUUNNALLA' THEN '"YKSI_KAISTA_VAHENEE_KAHDELLA_AJOSUUNNALLA"'::jsonb
-                                        WHEN 'VAHENTAA_SAMANAIKAISESTI_USEITA_KAISTOJA_KAHDELLA_AJOSUUNNALLA' THEN '"USEITA_KAISTOJA_VAHENEE_AJOSUUNNILLA"'::jsonb
-                                        WHEN 'VAHENTAA_SAMANAIKAISESTI_USEITA_KAISTOJA_LIITTYMIEN_ERI_SUUNNILLA' THEN '"USEITA_AJOSUUNTIA_POISTUU_KAYTOSTA"'::jsonb
-                                        ELSE area->'kaistahaitta'
-                                    END
-                                )
-                            ELSE area
+UPDATE applications
+SET applicationData = jsonb_set(
+    applicationData,
+    '{areas}',
+    (
+        SELECT jsonb_agg(
+            CASE
+                WHEN (area #>> '{kaistahaitta}') IS NOT NULL THEN
+                    jsonb_set(
+                        area,
+                        '{kaistahaitta}',
+                        CASE area->>'kaistahaitta'
+                            WHEN 'VAHENTAA_KAISTAN_YHDELLA_AJOSUUNNALLA' THEN '"YKSI_KAISTA_VAHENEE"'::jsonb
+                            WHEN 'VAHENTAA_SAMANAIKAISESTI_KAISTAN_KAHDELLA_AJOSUUNNALLA' THEN '"YKSI_KAISTA_VAHENEE_KAHDELLA_AJOSUUNNALLA"'::jsonb
+                            WHEN 'VAHENTAA_SAMANAIKAISESTI_USEITA_KAISTOJA_KAHDELLA_AJOSUUNNALLA' THEN '"USEITA_KAISTOJA_VAHENEE_AJOSUUNNILLA"'::jsonb
+                            WHEN 'VAHENTAA_SAMANAIKAISESTI_USEITA_KAISTOJA_LIITTYMIEN_ERI_SUUNNILLA' THEN '"USEITA_AJOSUUNTIA_POISTUU_KAYTOSTA"'::jsonb
+                            ELSE area->'kaistahaitta'
                         END
                     )
-                )
-            ELSE applicationData
-        END AS updated_data
-    FROM applications,
-         jsonb_array_elements(applicationData->'areas') AS area
-    WHERE applicationData IS NOT NULL
-    GROUP BY id, applicationData
+                ELSE area
+            END
+        )
+        FROM jsonb_array_elements(applicationData->'areas') AS area
+    )
 )
-
-UPDATE applications
-SET applicationData = updated_areas.updated_data
-    FROM updated_areas
-WHERE applications.id = updated_areas.id;
+WHERE applicationType = 'EXCAVATION_NOTIFICATION'
+  AND (applicationData #>> '{areas}') IS NOT NULL
+  AND jsonb_typeof(applicationData->'areas') = 'array';
