cd458e9c6a62b4f3e8c395fc85e39517f1dde0f0
Topias Heinonen <topias.heinonen@gofore.com>
2024-11-29 15:02:32 +0200

    HAI-1302 Delete täydennyspyyntö and päätös when unlinking applications (#886)
    When unlinking hakemukset from Allu, they return to the draft state. It
    doesn't make any sense to have täydennyspyyntö or päätös for a hakemus
    that's a draft. So we can delete the related objects.
    
    For täydennyspyyntö, it's also important because Allu reuses their ID
    after a reset so they might cause conflicts.
    
    Also, delete the täydennys attachments and päätökset from Blob Storage.

--- services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/factory/TaydennysAttachmentBuilder.kt
+++ services/hanke-service/src/test/kotlin/fi/hel/haitaton/hanke/factory/TaydennysAttachmentBuilder.kt
@@ -1,10 +1,24 @@
 package fi.hel.haitaton.hanke.factory
 
+import fi.hel.haitaton.hanke.attachment.FILE_NAME_PDF
+import fi.hel.haitaton.hanke.attachment.PDF_BYTES
 import fi.hel.haitaton.hanke.attachment.common.TaydennysAttachmentEntity
 import fi.hel.haitaton.hanke.attachment.common.TaydennysAttachmentRepository
+import org.springframework.http.MediaType
 
 data class TaydennysAttachmentBuilder(
     val value: TaydennysAttachmentEntity,
     val attachmentRepository: TaydennysAttachmentRepository,
     val attachmentFactory: TaydennysAttachmentFactory,
-)
+) {
+    fun withContent(
+        filename: String = FILE_NAME_PDF,
+        mediaType: MediaType = MediaType.APPLICATION_PDF,
+        bytes: ByteArray = PDF_BYTES,
+    ): TaydennysAttachmentBuilder {
+        value.size = bytes.size.toLong()
+        attachmentRepository.save(value)
+        attachmentFactory.saveContent(value.blobLocation, filename, mediaType, bytes)
+        return this
+    }
+}
