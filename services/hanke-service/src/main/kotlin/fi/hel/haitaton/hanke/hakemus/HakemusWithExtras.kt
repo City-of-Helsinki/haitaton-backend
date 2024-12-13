e43b95278e714fcf0e727a6a2260b7e83118a25d
Topias Heinonen <topias.heinonen@gofore.com>
2024-12-13 10:42:22 +0200

    HAI-3326 Add täydennys liitteet to hakemus API (#901)
    When getting a single hakemus, add liitteet to the taydennys object of
    the hakemus. Return the information for any existing attachments.

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/hakemus/HakemusWithExtras.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/hakemus/HakemusWithExtras.kt
@@ -3,8 +3,8 @@ package fi.hel.haitaton.hanke.hakemus
 import com.fasterxml.jackson.annotation.JsonUnwrapped
 import fi.hel.haitaton.hanke.paatos.Paatos
 import fi.hel.haitaton.hanke.paatos.PaatosResponse
-import fi.hel.haitaton.hanke.taydennys.TaydennysWithMuutokset
-import fi.hel.haitaton.hanke.taydennys.TaydennysWithMuutoksetResponse
+import fi.hel.haitaton.hanke.taydennys.TaydennysWithExtras
+import fi.hel.haitaton.hanke.taydennys.TaydennysWithExtrasResponse
 import fi.hel.haitaton.hanke.taydennys.Taydennyspyynto
 import fi.hel.haitaton.hanke.taydennys.TaydennyspyyntoResponse
 
@@ -12,7 +12,7 @@ data class HakemusWithExtras(
     val hakemus: Hakemus,
     val paatokset: List<Paatos>,
     val taydennyspyynto: Taydennyspyynto?,
-    val taydennys: TaydennysWithMuutokset?,
+    val taydennys: TaydennysWithExtras?,
 ) {
     fun toResponse(): HakemusWithExtrasResponse =
         HakemusWithExtrasResponse(
@@ -27,5 +27,5 @@ data class HakemusWithExtrasResponse(
     @JsonUnwrapped val hakemus: HakemusResponse,
     val paatokset: Map<String, List<PaatosResponse>>,
     val taydennyspyynto: TaydennyspyyntoResponse?,
-    val taydennys: TaydennysWithMuutoksetResponse?,
+    val taydennys: TaydennysWithExtrasResponse?,
 )
