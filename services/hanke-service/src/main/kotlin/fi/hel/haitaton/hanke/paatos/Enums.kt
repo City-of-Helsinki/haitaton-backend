673a1e3a72c04038a1b2fb984796e6aad8a1d7c5
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2025-07-22 16:19:21 +0300

    HAI-3587 Fix handling pending Allu events even when there are no new ones. Fix duplicate key error when päätös gets reverted (a new päätös event is received).

--- services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/paatos/Enums.kt
+++ services/hanke-service/src/main/kotlin/fi/hel/haitaton/hanke/paatos/Enums.kt
@@ -1,9 +1,24 @@
 package fi.hel.haitaton.hanke.paatos
 
+import fi.hel.haitaton.hanke.allu.ApplicationStatus
+
 enum class PaatosTyyppi {
     PAATOS,
     TOIMINNALLINEN_KUNTO,
-    TYO_VALMIS,
+    TYO_VALMIS;
+
+    companion object {
+        fun valueOfApplicationStatus(applicationStatus: ApplicationStatus): PaatosTyyppi =
+            when (applicationStatus) {
+                ApplicationStatus.DECISION -> PAATOS
+                ApplicationStatus.OPERATIONAL_CONDITION -> TOIMINNALLINEN_KUNTO
+                ApplicationStatus.FINISHED -> TYO_VALMIS
+                else ->
+                    throw IllegalArgumentException(
+                        "Unsupported application status: $applicationStatus"
+                    )
+            }
+    }
 }
 
 enum class PaatosTila {
