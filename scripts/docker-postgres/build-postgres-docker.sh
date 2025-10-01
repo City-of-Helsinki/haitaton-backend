f3d90956891b98bbdffdd34ccd7bf4da71f7c373
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2025-10-01 12:53:29 +0300

    HAI-3597 Changed PostgreSQL version from 13 to 18 for local development

--- scripts/docker-postgres/build-postgres-docker.sh
+++ scripts/docker-postgres/build-postgres-docker.sh
@@ -2,4 +2,4 @@ docker build -t haitaton-postgres .
 #remove old container if exists
 docker rm -f haitaton-postgres > /dev/null 2>&1 && echo 'removed old haitaton container' || echo 'nothing to remove'
 #add your own data path and port, I use 5433 here and ~/haitaton-data
-docker run -d --rm --name haitaton-postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 --volume ~/haitaton-data:/var/lib/postgresql/data haitaton-postgres
+docker run -d --rm --name haitaton-postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 --volume ~/haitaton-data:/var/lib/postgresql/18/data haitaton-postgres
